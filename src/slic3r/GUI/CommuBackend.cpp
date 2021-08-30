c21a35d1b3889608d8761fc47825e620dc19f6fa
stone.li <stone.li@bambulab.com>
2021-08-30 08:51:32 +0800

    FIX: fix type of publish payload in mqtt
    Change-Id: I1ef3fcd710c318391762865f37fcb55e33b7195e
    Signed-off-by: stone.li <stone.li@bambulab.com>
    (cherry picked from commit 16132eb7836c11fe3502782c52bcba1cf3c2af94)

--- src/slic3r/GUI/CommuBackend.cpp
+++ src/slic3r/GUI/CommuBackend.cpp
@@ -383,7 +383,7 @@ namespace Slic3r {
             // !!!blocking in is_connected() !!!
             //if (m_mqtt_cli->is_connected()) {
                 std::string topic = get_request_topic(device_id);
-                mqtt::message_ptr pubmsg = mqtt::make_message(topic, json_str);
+                mqtt::message_ptr pubmsg = mqtt::make_message(topic, json_str.c_str());
                 pubmsg->set_qos(0);
                 BOOST_LOG_TRIVIAL(trace) << "CommuBackend::publish_json_to_client start 1";
                 mqtt::delivery_token_ptr token = m_mqtt_cli->publish(pubmsg);
