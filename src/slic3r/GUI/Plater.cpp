e56ccc9b45f7059b90f7be896b7393eb3bae6581
yifan.wu <yifan.wu@bambulab.com>
2022-01-17 17:45:29 +0800

    ENH: enable slice button when wipe volume changes
    Signed-off-by: yifan.wu <yifan.wu@bambulab.com>
    Change-Id: I73beaaa711528d2ada5d8b3777f19f1ec74c4132

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -2222,7 +2222,8 @@ Plater::priv::priv(Plater *q, MainFrame *main_frame, AccountManager* acc)
         // Preset change event
         sidebar->Bind(wxEVT_COMBOBOX, &priv::on_select_preset, this);
         sidebar->Bind(EVT_OBJ_LIST_OBJECT_SELECT, [this](wxEvent&) { priv::selection_changed(); });
-        sidebar->Bind(EVT_SCHEDULE_BACKGROUND_PROCESS, [this](SimpleEvent&) { this->schedule_background_process(); });
+        // BBS: should bind BACKGROUND_PROCESS event to plater
+        q->Bind(EVT_SCHEDULE_BACKGROUND_PROCESS, [this](SimpleEvent&) { this->schedule_background_process(); });
         // jump to found option from SearchDialog
         q->Bind(wxCUSTOMEVT_JUMP_TO_OPTION, [this](wxCommandEvent& evt) { sidebar->jump_to_option(evt.GetInt()); });
     }
