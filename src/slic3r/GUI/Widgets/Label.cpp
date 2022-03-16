134bf8bfcf982a43304a92852a60f7946f428f7a
Tao Wang <tao.wang@bambulab.com>
2022-03-16 12:28:44 +0800

    Fix:label: use previous version
    Change-Id: Ie7c61fdfee041fc6ffee353d5b6d381efabf4d79

--- src/slic3r/GUI/Widgets/Label.cpp
+++ src/slic3r/GUI/Widgets/Label.cpp
@@ -1,22 +1,23 @@
 #include "Label.hpp"
+#include "StaticBox.hpp"
 
-static wxFont sysFont(int size, bool bold) {
+static wxFont sysFont(int size, bool bold)
+{
 #ifdef __linux__
-	return wxFont{};
+    return wxFont{};
 #endif
 #ifdef __WIN32__
-	size = size * 4 / 5;
+    size = size * 4 / 5;
 #endif
-	auto face = wxString::FromUTF8("HarmonyOS Sans SC");
-	wxFont font{size, wxFONTFAMILY_SWISS, wxNORMAL, bold ? wxBOLD : wxNORMAL, false, face };
-	font.SetFaceName(face);
-	if (!font.IsOk()) {
-		font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
-		if (bold)
-			font.MakeBold();
-		font.SetPointSize(size);
-	}
-	return font;
+    auto   face = wxString::FromUTF8("HarmonyOS Sans SC");
+    wxFont font{size, wxFONTFAMILY_SWISS, wxNORMAL, bold ? wxBOLD : wxNORMAL, false, face};
+    font.SetFaceName(face);
+    if (!font.IsOk()) {
+        font = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
+        if (bold) font.MakeBold();
+        font.SetPointSize(size);
+    }
+    return font;
 }
 wxFont Label::Head_24 = sysFont(24, true);
 wxFont Label::Head_20 = sysFont(20, true);
@@ -31,20 +32,12 @@ wxFont Label::Body_13 = sysFont(13, false);
 wxFont Label::Body_12 = sysFont(12, false);
 wxFont Label::Body_10 = sysFont(10, false);
 
-Label::Label(wxString const & text, wxWindow* parent)
-	: wxStaticText(parent, wxID_ANY, text, wxDefaultPosition, wxDefaultSize, 0)
-{
-	SetFont(Body_16);
-}
+Label::Label(wxString const &text, wxWindow *parent) : Label(Body_16, text, parent) {}
 
-Label::Label(wxFont const& font, wxWindow* parent)
-	: wxStaticText(parent, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, 0)
-{
-	SetFont(font);
-}
+Label::Label(wxFont const &font, wxWindow *parent) : Label(font, "", parent) {}
 
-Label::Label(wxFont const& font, wxString const& text, wxWindow* parent)
-	: wxStaticText(parent, wxID_ANY, text, wxDefaultPosition, wxDefaultSize, 0)
+Label::Label(wxFont const &font, wxString const &text, wxWindow *parent) : wxStaticText(parent, wxID_ANY, text, wxDefaultPosition, wxDefaultSize, 0)
 {
-	SetFont(font);
+    SetBackgroundColour(StaticBox::GetParentBackgroundColor(parent));
+    SetFont(font);
 }
