7ae74aeca15e6a68977e125da66dd00b12ed24c3
Arthur <arthur.tang@bambulab.com>
2022-03-21 16:09:32 +0800

    FIX: arrange by object not working due to wrong vitrify_temp
    Change-Id: I13fa31dda894a1bef575ed9d5b1eb9c5638ab34f

--- src/libslic3r/ModelArrange.cpp
+++ src/libslic3r/ModelArrange.cpp
@@ -140,9 +140,12 @@ ArrangePolygon get_instance_arrange_poly(ModelInstance* instance, const Slic3r::
         ap.print_temp = config.opt_int("nozzle_temperature", ap.extrude_id - 1);
     if (config.has("nozzle_temperature_initial_layer")) //get the nozzle_temperature_initial_layer
         ap.first_print_temp = config.opt_int("nozzle_temperature_initial_layer", ap.extrude_id - 1);
-    if (config.has("temperature_vitrification"))
-        ap.vitrify_temp = config.opt_int("temperature_vitrification", ap.extrude_id - 1);
-
+    // BBS: since first_bed_temp packs all 3 temperatures, vitrify_temp should follow same routine
+    if (config.has("temperature_vitrification")) {
+        int tmp = config.opt_int("temperature_vitrification", ap.extrude_id - 1);
+        for (int i = 0; i < BedType::btCount; i++)
+            ap.vitrify_temp += tmp * pow(100, BedType::btCount - i - 1);
+    }
     ap.height = instance->get_object()->bounding_box().size().z();
     ap.name = instance->get_object()->name;
     return ap;
