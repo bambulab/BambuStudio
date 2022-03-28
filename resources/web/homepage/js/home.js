8a3797e43cadcf3f9f3240fdfdf693fad032f703
zorro.zhang <zorro.zhang@bambulab.com>
2022-03-28 18:07:06 +0800

    ENH: Update MultiLanguage Text
    Change-Id: I7308c066e2954c34d525ef06c40e4370f527345d

--- resources/web/homepage/js/home.js
+++ resources/web/homepage/js/home.js
@@ -1,4 +1,15 @@
-function HandleStuido( pVal )
+/*var TestData={"sequence_id":"0","command":"studio_send_recentfile","data":[{"path":"D:\\work\\Models\\Toy\\3d-puzzle-cube-model_files\\3d-puzzle-cube.3mf","time":"2022\/3\/24 20:33:10"},{"path":"D:\\work\\Models\\Art\\Carved Stone Vase - remeshed+drainage\\Carved Stone Vase.3mf","time":"2022\/3\/24 17:11:51"},{"path":"D:\\work\\Models\\Art\\Kity & Cat\\Cat.3mf","time":"2022\/3\/24 17:07:55"},{"path":"D:\\work\\Models\\Toy\\鐩村墤.3mf","time":"2022\/3\/24 17:06:02"},{"path":"D:\\work\\Models\\Toy\\minimalistic-dual-tone-whistle-model_files\\minimalistic-dual-tone-whistle.3mf","time":"2022\/3\/22 21:12:22"},{"path":"D:\\work\\Models\\Toy\\spiral-city-model_files\\spiral-city.3mf","time":"2022\/3\/22 18:58:37"},{"path":"D:\\work\\Models\\Toy\\impossible-dovetail-puzzle-box-model_files\\impossible-dovetail-puzzle-box.3mf","time":"2022\/3\/22 20:08:40"}]};*/
+
+
+function OnInit()
+{	
+    TranslatePage();
+	
+	$("#HotspotWEB").prop("src","https://www.bambulab.com");
+}
+
+
+function HandleStudio( pVal )
 {
 	let strCmd = pVal['command'];
 	//alert(strCmd);
@@ -107,9 +118,9 @@ function ShowRecentFileList( pList )
 		let index=sPath.lastIndexOf('\\')>0?sPath.lastIndexOf('\\'):sPath.lastIndexOf('\/');
 		let sShortName=sPath.substring(index+1,sPath.length);
 		
-		let TmpHtml='<div class="FileItem" onClick="OnOpenRecentFile("'+sPath+'")">'+
+		let TmpHtml='<div class="FileItem"  onClick="OnOpenRecentFile(\''+ encodeURI(sPath)+'\')"  >'+
 				'<a class="FileTip" title="'+sPath+'"></a>'+
-				'<div class="FileImg"><img src="..\..\img\temp\\'+sImg+'"/></div>'+
+				'<div class="FileImg" ><img src="..\..\img\temp\\'+sImg+'" onerror="this.onerror=null;this.src=\'img/d.png\';"  alt="No Image"  /></div>'+
 				'<a>'+sShortName+'</a>'+
 				'<div class="FileDate">'+sTime+'</div>'+
 			    '</div>';
@@ -157,7 +168,7 @@ function OnOpenRecentFile( strPath )
 	tSend['sequence_id']=Math.round(new Date() / 1000);
 	tSend['command']="homepage_open_recentfile";
 	tSend['data']={};
-	tSend['data']['path']=strPath;
+	tSend['data']['path']=decodeURI(strPath);
 	
 	SendWXMessage( JSON.stringify(tSend) );	
 }
