b1479d75c1f9810e3d9d65eb20131945ecdccba1
lane.wei <lane.wei@bambulab.com>
2022-07-15 15:50:02 +0800

    ENH: remove the bambu_networking and bambu player from deps
    Change-Id: I7815b13edabd513a94ccbe2d4dfd8d0363ab2b7e

--- deps/BambuNetworking/BambuNetworking.cmake
+++
@@ -1,7 +0,0 @@
-bambustudio_add_cmake_project(BambuNetworking
-    GIT_REPOSITORY "https://github.com/bambulab/BambuNetworking"
-    GIT_TAG v0.0.1
-    #DEPENDS dep_Boost
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${DESTDIR}/usr/local
-)
