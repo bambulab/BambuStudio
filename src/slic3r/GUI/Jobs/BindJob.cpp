b21d28e4334cda4338acd414cc1b7098d3f96bd6
Stone Li <stone.li@bambulab.com>
2022-07-17 18:25:34 +0800

    FIX: fix 100% display error in BBLStatusBarSend/Bind
    Change-Id: Id848cd07a7a367c47a28dfbd489a0ab22510aa25
    Signed-off-by: Stone Li <stone.li@bambulab.com>

--- src/slic3r/GUI/Jobs/BindJob.cpp
+++ src/slic3r/GUI/Jobs/BindJob.cpp
@@ -99,6 +99,8 @@ void BindJob::process()
             } else if (stage == BBL::BindJobStage::LoginStageFinished) {
                 curr_percent = 100;
                 msg = _L("Logging in");
+            } else {
+                msg = _L("Logging in");
             }
             if (code != 0) {
                 msg = login_failed_str + wxString::Format("(code=%d,info=%s)", code, info);
