7a078e5eea0008b51d0d84621c01e54f139d5c99
Stone Li <stone.li@bambulab.com>
2022-07-22 12:37:06 +0800

    ENH: display finished stage
    Change-Id: I77dc88c489ec685d5722b70c4bb10e2cc075aefb

--- src/slic3r/GUI/StatusPanel.cpp
+++ src/slic3r/GUI/StatusPanel.cpp
@@ -1536,7 +1536,7 @@ void StatusPanel::update_subtask(MachineObject *obj)
 
     if (obj->is_system_printing()) {
         reset_printing_values();
-    } else if (obj->is_in_printing()) {
+    } else if (obj->is_in_printing() || obj->is_printing_finished()) {
         if (obj->is_in_prepare()) {
             m_button_abort->Enable(false);
             m_button_pause_resume->Enable(false);
