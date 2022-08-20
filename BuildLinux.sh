7f66d344cdc0b80e3bdabe8a03a904cab0964081
DeftDawg <DeftDawg@gmail.com>
2022-08-20 10:16:04 -0400

    * Update BuildLinux.sh to work better in container build

--- BuildLinux.sh
+++ BuildLinux.sh
@@ -61,14 +61,15 @@ then
     mkdir build
 fi
 
-# Addtional Dev packages for BambuStudio 
+# Addtional Dev packages for BambuStudio
 export REQUIRED_DEV_PACKAGES="libmspack-dev libgstreamerd-3-dev libsecret-1-dev libwebkit2gtk-4.0-dev libosmesa6-dev libssl-dev libcurl4-openssl-dev eglexternalplatform-dev libudev-dev libdbus-1-dev"
 # libwebkit2gtk-4.1-dev ??
 export DEV_PACKAGES_COUNT=$(echo ${REQUIRED_DEV_PACKAGES} | wc -w)
 if [ $(dpkg --get-selections | grep -E "$(echo ${REQUIRED_DEV_PACKAGES} | tr ' ' '|')" | wc -l) -lt ${DEV_PACKAGES_COUNT} ]; then
-    sudo apt install -y ${REQUIRED_DEV_PACKAGES} git cmake
+    sudo apt install -y ${REQUIRED_DEV_PACKAGES} git cmake wget
 fi
 
+#FIXME: require root for -u option
 if [[ -n "$UPDATE_LIB" ]]
 then
     echo -n -e "Updating linux ...\n"
@@ -77,21 +78,21 @@ then
     if [[ -z "$FOUND_GTK3" ]]
     then
         echo -e "\nInstalling: libgtk2.0-dev libglew-dev libudev-dev libdbus-1-dev cmake git\n"
-        apt install libgtk2.0-dev libglew-dev libudev-dev libdbus-1-dev cmake git
+        apt install -y libgtk2.0-dev libglew-dev libudev-dev libdbus-1-dev cmake git
     else
         echo -e "\nFind libgtk-3, installing: libgtk-3-dev libglew-dev libudev-dev libdbus-1-dev cmake git\n"
-        apt install libgtk-3-dev libglew-dev libudev-dev libdbus-1-dev cmake git
+        apt install -y libgtk-3-dev libglew-dev libudev-dev libdbus-1-dev cmake git
     fi
     # for ubuntu 22.04:
     ubu_version="$(cat /etc/issue)" 
     if [[ $ubu_version == "Ubuntu 22.04"* ]]
     then
-        apt install curl libssl-dev libcurl4-openssl-dev m4
+        apt install -y curl libssl-dev libcurl4-openssl-dev m4
     fi
     if [[ -n "$BUILD_DEBUG" ]]
     then
         echo -e "\nInstalling: libssl-dev libcurl4-openssl-dev\n"
-        apt install libssl-dev libcurl4-openssl-dev
+        apt install -y libssl-dev libcurl4-openssl-dev
     fi
     echo -e "done\n"
     exit 0
@@ -209,6 +210,7 @@ then
     echo "done"
 fi
 
+if [[ -e $ROOT/build/src/BuildLinuxImage.sh ]]; then
 # Give proper permissions to script
 chmod 755 $ROOT/build/src/BuildLinuxImage.sh
 
@@ -222,3 +224,4 @@ echo "[9/9] Generating Linux app..."
         fi
     popd
 echo "done"
+fi
