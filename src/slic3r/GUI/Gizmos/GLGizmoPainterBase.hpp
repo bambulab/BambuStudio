7542a791db586a23baf9624c86f1b4ee64df8398
lane.wei <lane.wei@bambulab.com>
2022-10-18 23:26:19 +0800

    Revert "ENH: refine the wireframe logic"
    This reverts commit 2d56f467dfb03585b5a7c0e32a24b2f0de8eb6e6.

--- src/slic3r/GUI/Gizmos/GLGizmoPainterBase.hpp
+++ src/slic3r/GUI/Gizmos/GLGizmoPainterBase.hpp
@@ -112,7 +112,6 @@ protected:
 
 // BBS
 struct TrianglePatch {
-    std::vector<float>  patch_vertices;
     std::vector<int> triangle_indices;
     std::vector<int> facet_indices;
     EnforcerBlockerType type = EnforcerBlockerType::NONE;
@@ -161,15 +160,11 @@ protected:
     void clear()
     {
         // BBS
-        this->m_vertices_VBO_ids.clear();
         this->m_triangle_indices_VBO_ids.clear();
         this->m_triangle_indices_sizes.clear();
 
         for (TrianglePatch& patch : this->m_triangle_patches)
-        {
-            patch.patch_vertices.clear();
             patch.triangle_indices.clear();
-        }
         this->m_triangle_patches.clear();
     }
 
@@ -179,7 +174,7 @@ protected:
         return this->m_triangle_indices_VBO_ids[triangle_indices_idx] != 0;
     }
 
-    //std::vector<float>          m_patch_vertices;
+    std::vector<float>          m_patch_vertices;
     std::vector<TrianglePatch>  m_triangle_patches;
 
     // When the triangle indices are loaded into the graphics card as Vertex Buffer Objects,
@@ -188,8 +183,7 @@ protected:
 
     // IDs of the Vertex Array Objects, into which the geometry has been loaded.
     // Zero if the VBOs are not sent to GPU yet.
-    //unsigned int                m_vertices_VBO_id{ 0 };
-    std::vector<unsigned int>   m_vertices_VBO_ids;
+    unsigned int                m_vertices_VBO_id{ 0 };
     std::vector<unsigned int>   m_triangle_indices_VBO_ids;
 
     std::vector<std::array<float, 4>> m_ebt_colors;
