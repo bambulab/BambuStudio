0249b88ee57f5c77f9eaaa7be4b9da1c917ef042
tao wang <tao.wang@bambulab.com>
2022-12-21 11:58:15 +0800

    ENH:wxStaticText set the default foreground colour
    Change-Id: Ifb03554848bc869f8605fb10945e7681147204d5
    (cherry picked from commit 4599444719d60c92e432d595de7cea2607572b1b)

--- src/slic3r/GUI/Widgets/Label.cpp
+++ src/slic3r/GUI/Widgets/Label.cpp
@@ -225,6 +225,7 @@ Label::Label(wxWindow *parent, wxFont const &font, wxString const &text, long st
 {
     this->font = font;
     SetFont(font);
+    SetForegroundColour(*wxBLACK);
     SetBackgroundColour(StaticBox::GetParentBackgroundColor(parent));
     SetForegroundColour("#262E30");
     if (style & LB_PROPAGATE_MOUSE_EVENT) {
