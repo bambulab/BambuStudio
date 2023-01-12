570c23c0a4f151488ae51c3b992cd66eb03d12cf
lane.wei <lane.wei@bambulab.com>
2023-01-12 14:10:28 +0800

    ENH: version: change to 1.4.2
    Change-Id: Ia1c6e2690b89f3b876c8c43556a7980be5923438

--- src/slic3r/Utils/bambu_networking.hpp
+++ src/slic3r/Utils/bambu_networking.hpp
@@ -40,7 +40,7 @@ namespace BBL {
 
 #define BAMBU_NETWORK_LIBRARY               "bambu_networking"
 #define BAMBU_NETWORK_AGENT_NAME            "bambu_network_agent"
-#define BAMBU_NETWORK_AGENT_VERSION         "01.04.01.01"
+#define BAMBU_NETWORK_AGENT_VERSION         "01.04.02.01"
 
 
 //iot preset type strings
