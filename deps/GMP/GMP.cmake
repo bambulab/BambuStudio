732ee3493160869a3a5f1d2436d40ff61e833668
lane.wei <lane.wei@bambulab.com>
2023-07-12 16:49:40 +0800

    ENH: GMP: change the src url to pass compile currently
    Change-Id: I344f14dded450c5a7549b2a94cafe46c25db9448

--- deps/GMP/GMP.cmake
+++ deps/GMP/GMP.cmake
@@ -43,8 +43,8 @@ else ()
     endif ()
 
     ExternalProject_Add(dep_GMP
-        URL https://gmplib.org/download/gmp/gmp-6.2.1.tar.bz2
-        URL_HASH SHA256=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
+        URL https://github.com/bambulab/gmp/archive/refs/tags/6.2.1.tar.gz
+        URL_HASH SHA256=39b382701ffe5c331bc3ae8e956e65a0d5a9d0ec1d11c2177dee57be56387625
         DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/GMP
         BUILD_IN_SOURCE ON 
         CONFIGURE_COMMAND  env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --enable-shared=no --enable-cxx=yes --enable-static=yes "--prefix=${DESTDIR}/usr/local" ${_gmp_build_tgt}
