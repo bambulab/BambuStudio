96a3e5ac492cc1a9a900f7045299a34e0d5a450c
Bastien Nocera <hadess@hadess.net>
2023-11-24 17:19:41 +0100

    deps: Build wxWidgets without mspack support
    We don't use wxWidgets' CHM support, so don't require libmspack's
    development libraries, and disable building wxWidgets with the support.

--- deps/wxWidgets/wxWidgets.cmake
+++ deps/wxWidgets/wxWidgets.cmake
@@ -45,6 +45,7 @@ bambustudio_add_cmake_project(wxWidgets
         ${_wx_edge}
         -DwxUSE_WEBVIEW_IE=OFF
         -DwxUSE_REGEX=builtin
+        -DwxUSE_LIBMSPACK=OFF
         -DwxUSE_LIBXPM=builtin
         -DwxUSE_LIBSDL=OFF
         -DwxUSE_XTEST=OFF
