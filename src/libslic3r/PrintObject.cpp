c5cd1042f4ec60017fda066fba3b6314f491adc7
Bastien Nocera <hadess@hadess.net>
2023-12-13 18:22:16 +0100

    libslic3r: Fix Slic3r::format declaration

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -29,6 +29,8 @@
 
 #include <Shiny/Shiny.h>
 
+#include "format.hpp"
+
 using namespace std::literals;
 
 //! macro used to mark string used at localization,
@@ -474,7 +476,7 @@ void PrintObject::generate_support_material()
                     {SharpTail,L("floating regions")},
                     {Cantilever,L("floating cantilever")},
                     {LargeOverhang,L("large overhangs")} };
-                std::string warning_message = format(L("It seems object %s has %s. Please re-orient the object or enable support generation."),
+                std::string warning_message = Slic3r::format(L("It seems object %s has %s. Please re-orient the object or enable support generation."),
                     this->model_object()->name, reasons[sntype]);
                 this->active_step_add_warning(PrintStateBase::WarningLevel::NON_CRITICAL, warning_message, PrintStateBase::SlicingNeedSupportOn);
             }
