56944c056ce32bb71db25922f5a2e56ab2922934
lane.wei <lane.wei@bambulab.com>
2023-12-19 16:39:11 +0800

    ENH: CLI: add mk information support
    JIRA: no jira
    Change-Id: Idd89b143d439de50d9f52eb8aec95b262d66875d

--- src/BambuStudio.cpp
+++ src/BambuStudio.cpp
@@ -913,7 +913,7 @@ int CLI::run(int argc, char **argv)
     /*BOOST_LOG_TRIVIAL(info) << "begin to setup params, argc=" << argc << std::endl;
     for (int index=0; index < argc; index++)
         BOOST_LOG_TRIVIAL(info) << "index="<< index <<", arg is "<< argv[index] <<std::endl;
-    int debug_argc = 14;
+    int debug_argc = 11;
     char *debug_argv[] = {
         "F:\work\projects\bambu_debug\bamboo_slicer\build_debug\src\Debug\bambu-studio.exe",
         "--debug=2",
@@ -924,11 +924,8 @@ int CLI::run(int argc, char **argv)
         "--export-3mf=output.3mf",
         "--filament-colour",
         "#FFFFFFFF;#0000FFFF;#00FF00FF;#FF0000FF;#00000000;#FFFF00FF",
-        "--allow-multicolor-oneplate=1",
-        "--allow-rotations=0",
-        "--avoid-extrusion-cali-region=1",
-        "--load-assemble-list",
-        "assemble_list.json"
+        "--slice=0",
+        "1.3mf"
         };
     if (! this->setup(debug_argc, debug_argv))*/
     if (!this->setup(argc, argv))
@@ -1058,7 +1055,7 @@ int CLI::run(int argc, char **argv)
     std::vector<std::string> current_filaments_name, current_filaments_system_name, current_inherits_group;
     DynamicPrintConfig load_process_config, load_machine_config;
     bool new_process_config_is_system = true, new_printer_config_is_system = true;
-    std::string pipe_name;
+    std::string pipe_name, makerlab_name, makerlab_version;
 
     // Read input file(s) if any.
     BOOST_LOG_TRIVIAL(info) << "Will start to read model file now, file count :" << m_input_files.size() << "\n";
@@ -1110,6 +1107,14 @@ int CLI::run(int argc, char **argv)
 #endif
         }
     }
+
+    ConfigOptionString* makerlab_name_option = m_config.option<ConfigOptionString>("makerlab_name");
+    if (makerlab_name_option)
+        makerlab_name = makerlab_name_option->value;
+
+    ConfigOptionString* makerlab_version_option = m_config.option<ConfigOptionString>("makerlab_version");
+    if (makerlab_version_option)
+        makerlab_version = makerlab_version_option->value;
 
     //skip model object map construct
     if (need_skip) {
@@ -5234,6 +5239,13 @@ int CLI::run(int argc, char **argv)
 #endif
 
         BOOST_LOG_TRIVIAL(info) << "will export 3mf to " << export_3mf_file << std::endl;
+        if (!makerlab_name.empty()) {
+            Model &model = m_models[0];
+
+            model.mk_name = makerlab_name;
+            model.mk_version = makerlab_version;
+            BOOST_LOG_TRIVIAL(info) << boost::format("mk_name %1%, mk_version %2%")%makerlab_name %makerlab_version;
+        }
         if (! this->export_project(&m_models[0], export_3mf_file, plate_data_list, project_presets, thumbnails, top_thumbnails, pick_thumbnails,
                                 calibration_thumbnails, plate_bboxes, &m_print_config, minimum_save, plate_to_slice - 1))
         {
