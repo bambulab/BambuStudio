3cdaa7c0b082ccc3afd18223241b72c454ccd8bb
wenjie.guo <wenjie.guo@bambulab.com>
2023-12-20 11:59:26 +0800

    FIX: do not show the background bitmap when model is white.
    Jira: STUDIO-5620
    
    Signed-off-by: wenjie.guo <wenjie.guo@bambulab.com>
    Change-Id: Ic61ba2ca53d071c815323b980abf79c067770b58
    (cherry picked from commit cc55e02e753e3ae88d1a0bd0d9fc51aab9ab7fd8)

--- src/slic3r/GUI/StatusPanel.hpp
+++ src/slic3r/GUI/StatusPanel.hpp
@@ -204,6 +204,7 @@ private:
     ProgressBar*    m_gauge_progress;
     Label* m_error_text;
     PrintingTaskType m_type;
+    int m_brightness_value{ -1 };
 
 public:
     void init_bitmaps();
@@ -225,6 +226,7 @@ public:
     void show_priting_use_info(bool show, wxString time = wxEmptyString, wxString weight = wxEmptyString);
     void show_profile_info(bool show, wxString profile = wxEmptyString);
     void set_thumbnail_img(const wxBitmap& bmp);
+    void set_brightness_value(int value) { m_brightness_value = value; }
     void set_plate_index(int plate_idx = -1);
     void market_scoring_show();
     void market_scoring_hide();
@@ -244,7 +246,6 @@ public:
     void set_star_count_dirty(bool dirty) { m_star_count_dirty = dirty; }
     void                           set_has_reted_text(bool has_rated);
     void paint(wxPaintEvent&);
-
 };
 
 class StatusBasePanel : public wxScrolledWindow
