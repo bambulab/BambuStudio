e4b991363b797f1789ec52514c2baebcf27ca65a
enricoturri1966 <enricoturri@seznam.cz>
2024-02-27 15:38:23 +0800

    NEW: upgrade GLModel and so on
    Jira: STUDIO-6166
    most of code from PrusaSlicer
    thanks for PrusaSlicer and enricoturri1966
    commit f8ce187262df8ef588f649c414a095c971f36e67
    Author: enricoturri1966 <enricoturri@seznam.cz>
    Date:   Wed Mar 9 11:51:02 2022 +0100
    
        Tech ENABLE_LEGACY_OPENGL_REMOVAL - Refactoring of GLModel to automatically detect the data type to use into the index buffer in dependence of vertices count
    
    Change-Id: I8b05c5c01c1d06cc24d1981479f44c016a46b2dd

--- src/slic3r/GUI/GLModel.hpp
+++ src/slic3r/GUI/GLModel.hpp
@@ -28,13 +28,110 @@ namespace GUI {
             LineLoop
         };
 
+        struct Geometry
+        {
+            // enum class EPrimitiveType : unsigned char { Points, Triangles, TriangleStrip, TriangleFan, Lines, LineStrip, LineLoop };
+            enum class EVertexLayout : unsigned char {
+                P2,     // position 2 floats
+                P2T2,   // position 2 floats + texture coords 2 floats
+                P3,     // position 3 floats
+                P3T2,   // position 3 floats + texture coords 2 floats
+                P3N3,   // position 3 floats + normal 3 floats
+                P3N3T2, // position 3 floats + normal 3 floats + texture coords 2 floats
+                P4,     // position 4 floats
+            };
+
+            enum class EIndexType : unsigned char {
+                UINT,   // unsigned int
+                USHORT, // unsigned short
+                UBYTE   // unsigned byte
+            };
+
+            struct Format
+            {
+                PrimitiveType type{PrimitiveType::Triangles};
+                EVertexLayout vertex_layout{EVertexLayout::P3N3};
+            };
+
+            Format                    format;
+            std::vector<float>        vertices;
+            std::vector<unsigned int> indices;
+            EIndexType                index_type{EIndexType::UINT};
+            ColorRGBA                 color{ColorRGBA::BLACK()};
+
+            void reserve_vertices(size_t vertices_count) { vertices.reserve(vertices_count * vertex_stride_floats(format)); }
+            void reserve_indices(size_t indices_count) { indices.reserve(indices_count); }
+
+            void add_vertex(const Vec2f &position);                                              // EVertexLayout::P2
+            void add_vertex(const Vec2f &position, const Vec2f &tex_coord);                      // EVertexLayout::P2T2
+            void add_vertex(const Vec3f &position);                                              // EVertexLayout::P3
+            void add_vertex(const Vec3f &position, const Vec2f &tex_coord);                      // EVertexLayout::P3T2
+            void add_vertex(const Vec3f &position, const Vec3f &normal);                         // EVertexLayout::P3N3
+            void add_vertex(const Vec3f &position, const Vec3f &normal, const Vec2f &tex_coord); // EVertexLayout::P3N3T2
+            void add_vertex(const Vec4f &position);                                              // EVertexLayout::P4
+
+            void set_vertex(size_t id, const Vec3f &position, const Vec3f &normal); // EVertexLayout::P3N3
+
+            void set_index(size_t id, unsigned int index);
+
+            void add_index(unsigned int id);
+            void add_line(unsigned int id1, unsigned int id2);
+            void add_triangle(unsigned int id1, unsigned int id2, unsigned int id3);
+
+            Vec2f extract_position_2(size_t id) const;
+            Vec3f extract_position_3(size_t id) const;
+            Vec3f extract_normal_3(size_t id) const;
+            Vec2f extract_tex_coord_2(size_t id) const;
+
+            unsigned int extract_index(size_t id) const;
+
+            void remove_vertex(size_t id);
+
+            bool is_empty() const { return vertices_count() == 0 || indices_count() == 0; }
+
+            size_t vertices_count() const { return vertices.size() / vertex_stride_floats(format); }
+            size_t indices_count() const { return indices.size(); }
+
+            size_t vertices_size_floats() const { return vertices.size(); }
+            size_t vertices_size_bytes() const { return vertices_size_floats() * sizeof(float); }
+            size_t indices_size_bytes() const { return indices.size() * index_stride_bytes(*this); }
+
+            indexed_triangle_set get_as_indexed_triangle_set() const;
+
+            static size_t vertex_stride_floats(const Format &format);
+            static size_t vertex_stride_bytes(const Format &format) { return vertex_stride_floats(format) * sizeof(float); }
+
+            static size_t position_stride_floats(const Format &format);
+            static size_t position_stride_bytes(const Format &format) { return position_stride_floats(format) * sizeof(float); }
+            static size_t position_offset_floats(const Format &format);
+            static size_t position_offset_bytes(const Format &format) { return position_offset_floats(format) * sizeof(float); }
+
+            static size_t normal_stride_floats(const Format &format);
+            static size_t normal_stride_bytes(const Format &format) { return normal_stride_floats(format) * sizeof(float); }
+            static size_t normal_offset_floats(const Format &format);
+            static size_t normal_offset_bytes(const Format &format) { return normal_offset_floats(format) * sizeof(float); }
+
+            static size_t tex_coord_stride_floats(const Format &format);
+            static size_t tex_coord_stride_bytes(const Format &format) { return tex_coord_stride_floats(format) * sizeof(float); }
+            static size_t tex_coord_offset_floats(const Format &format);
+            static size_t tex_coord_offset_bytes(const Format &format) { return tex_coord_offset_floats(format) * sizeof(float); }
+
+            static size_t index_stride_bytes(const Geometry &data);
+
+            static bool has_position(const Format &format);
+            static bool has_normal(const Format &format);
+            static bool has_tex_coord(const Format &format);
+        };
+
         struct RenderData
         {
-            PrimitiveType type;
-            unsigned int vbo_id{ 0 };
-            unsigned int ibo_id{ 0 };
-            size_t indices_count{ 0 };
-            std::array<float, 4> color{ 1.0f, 1.0f, 1.0f, 1.0f };
+            Geometry geometry;
+
+            PrimitiveType        type;
+            unsigned int         vbo_id{0};
+            unsigned int         ibo_id{0};
+            size_t               indices_count{0};
+            std::array<float, 4> color{1.0f, 1.0f, 1.0f, 1.0f};
         };
 
         struct InitializationData
@@ -66,8 +163,11 @@ namespace GUI {
 
     public:
         GLModel() = default;
-        virtual ~GLModel() { reset(); }
+        virtual ~GLModel();
 
+        TriangleMesh *mesh{nullptr};
+
+        void init_from(Geometry &&data, bool generate_mesh = false);
         void init_from(const InitializationData& data);
         void init_from(const indexed_triangle_set& its, const BoundingBoxf3& bbox);
         void init_from(const indexed_triangle_set& its);
@@ -87,7 +187,7 @@ namespace GUI {
         const std::string& get_filename() const { return m_filename; }
 
     private:
-        void send_to_gpu(RenderData& data, const std::vector<float>& vertices, const std::vector<unsigned int>& indices);
+        bool send_to_gpu(RenderData &data, const std::vector<float> &vertices, const std::vector<unsigned int> &indices) const;
     };
 
     // create an arrow with cylindrical stem and conical tip, with the given dimensions and resolution
@@ -113,7 +213,19 @@ namespace GUI {
     // the diamond is contained into a box with size [1, 1, 1]
     GLModel::InitializationData diamond(int resolution);
 
-} // namespace GUI
+    // create a sphere with smooth normals
+    // the origin of the sphere is in its center
+    GLModel::Geometry smooth_sphere(unsigned int resolution, float radius);
+    // create a cylinder with smooth normals
+    // the axis of the cylinder is the Z axis
+    // the origin of the cylinder is the center of its bottom cap face
+    GLModel::Geometry smooth_cylinder(unsigned int resolution, float radius, float height);
+    // create a torus with smooth normals
+    // the axis of the torus is the Z axis
+    // the origin of the torus is in its center
+    GLModel::Geometry smooth_torus(unsigned int primary_resolution, unsigned int secondary_resolution, float radius, float thickness);
+
+    } // namespace GUI
 } // namespace Slic3r
 
 #endif // slic3r_GLModel_hpp_
