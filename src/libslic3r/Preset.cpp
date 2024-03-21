3c92639353a05c98f8df64a84fe9c0e87acdd525
Leon Fisher-Skipper <47602359+LeonFisherSkipper@users.noreply.github.com>
2024-03-21 12:16:38 +0800

    NEW:reducing purge through retracting filament
    1.reducing purge through retracting filament.Currently only
    applicable to X&P series
    
    github: PR#3100
    
    Signed-off-by: XunZhangBambu <xun.zhang@bambulab.com>
    Change-Id: Ie328039872e50e699dc5e5082fa99f68ac5f5fd1

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -899,7 +899,8 @@ static std::vector<std::string> s_Preset_printer_options {
     "print_host_webui",
     "printhost_cafile","printhost_port","printhost_authorization_type",
     "printhost_user", "printhost_password", "printhost_ssl_ignore_revoke",
-    "use_relative_e_distances", "extruder_type","use_firmware_retraction"
+    "use_relative_e_distances", "extruder_type","use_firmware_retraction",
+    "long_retraction_when_cut","retraction_distance_when_cut","enable_long_retraction_when_cut"
 };
 
 static std::vector<std::string> s_Preset_sla_print_options {
