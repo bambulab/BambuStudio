76e3e210e0d22592172e236609f83fcbebea65ea
BBL\chuan.he <chuan.he@bambulab.com>
2024-06-13 14:54:35 +0800

    feat:add more slice time details to json file
    Change-Id: I6c29658a051144dda772b42603db6585ea58816b
    jira: none
    (cherry picked from commit d1e4b98db6ed0f8e1e29d7e8bdcdda96948d10dd)

--- src/libslic3r/SLAPrint.cpp
+++ src/libslic3r/SLAPrint.cpp
@@ -686,7 +686,7 @@ bool SLAPrint::invalidate_step(SLAPrintStep step)
     return invalidated;
 }
 
-void SLAPrint::process(long long *time_cost_with_cache, bool use_cache)
+void SLAPrint::process(std::unordered_map<std::string, long long>* slice_time, bool use_cache)
 {
     if (m_objects.empty())
         return;
