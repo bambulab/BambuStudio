4b23b42b2238efecf81439b6cf20e68d12648f89
qing.zhang <qing.zhang@bambulab.com>
2024-06-25 17:49:17 +0800

    ENH: ironing inset
    Jira: 7391
    
    Signed-off-by: qing.zhang <qing.zhang@bambulab.com>
    Change-Id: I2233b267ad7e3eb22ed9c232a89b7679173c7b34

--- src/slic3r/GUI/ConfigManipulation.cpp
+++ src/slic3r/GUI/ConfigManipulation.cpp
@@ -686,7 +686,7 @@ void ConfigManipulation::toggle_print_fff_options(DynamicPrintConfig *config, co
 
     bool has_ironing = (config->opt_enum<IroningType>("ironing_type") != IroningType::NoIroning);
     for (auto el : {
-        "ironing_pattern","ironing_speed", "ironing_flow", "ironing_spacing", "ironing_direction"})
+        "ironing_pattern","ironing_speed", "ironing_flow", "ironing_spacing", "ironing_direction", "ironing_inset"})
         toggle_line(el, has_ironing);
 
     // bool have_sequential_printing = (config->opt_enum<PrintSequence>("print_sequence") == PrintSequence::ByObject);
