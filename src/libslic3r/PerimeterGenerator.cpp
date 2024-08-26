93e2d684128484a3269fad56d1aa59617165cd1a
Bastien Nocera <hadess@hadess.net>
2024-08-26 22:32:36 +0200

    Fix missing std::thread declaration
    /run/build/BambuStudio/src/libslic3r/PerimeterGenerator.cpp: In function ‘double Slic3r::random_value()’:
    /run/build/BambuStudio/src/libslic3r/PerimeterGenerator.cpp:31:76: error: ‘thread’ is not a member of ‘std’
       31 |     thread_local std::mt19937 gen(rd.entropy() > 0 ? rd() : std::hash<std::thread::id>()(std::this_thread::get_id()));
          |                                                                            ^~~~~~
    /run/build/BambuStudio/src/libslic3r/PerimeterGenerator.cpp:15:1: note: ‘std::thread’ is defined in header ‘<thread>’; did you forget to ‘#include <thread>’?
       14 | #include "libslic3r/AABBTreeLines.hpp"
      +++ |+#include <thread>
       15 | static const int overhang_sampling_number = 6;

--- src/libslic3r/PerimeterGenerator.cpp
+++ src/libslic3r/PerimeterGenerator.cpp
@@ -10,6 +10,7 @@
 #include <cmath>
 #include <cassert>
 #include <random>
+#include <thread>
 #include <unordered_set>
 #include "libslic3r/AABBTreeLines.hpp"
 static const int overhang_sampling_number = 6;
