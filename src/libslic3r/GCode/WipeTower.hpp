0e5cc29564923ac12992c0d4ba4b16aea1933dd5
zhimin.zeng <zhimin.zeng@bambulab.com>
2024-09-04 09:38:16 +0800

    ENH: support tpu for multi_extruder
    jira: none
    Change-Id: I556804aadac00406e7214b86f7925170a90c55ce

--- src/libslic3r/GCode/WipeTower.hpp
+++ src/libslic3r/GCode/WipeTower.hpp
@@ -259,6 +259,9 @@ public:
 
 	void set_filament_map(const std::vector<int> &filament_map) { m_filament_map = filament_map; }
 
+	void set_has_tpu_filament(bool has_tpu) { m_has_tpu_filament = has_tpu; }
+    bool has_tpu_filament() const { return m_has_tpu_filament; }
+
     struct FilamentParameters {
         std::string 	    material = "PLA";
         bool                is_soluble = false;
@@ -320,6 +323,8 @@ private:
     size_t       m_cur_layer_id;
     NozzleChangeResult m_nozzle_change_result;
     std::vector<int>   m_filament_map;
+    bool               m_has_tpu_filament{false};
+    bool               m_is_multi_extruder{false};
 
 	// G-code generator parameters.
     // BBS: remove useless config
@@ -384,6 +389,8 @@ private:
 
     // Goes through m_plan, calculates border and finish_layer extrusions and subtracts them from last wipe
     void save_on_last_wipe();
+
+	bool is_tpu_filament(int filament_id) const;
 
 	// BBS
 	box_coordinates align_perimeter(const box_coordinates& perimeter_box);
