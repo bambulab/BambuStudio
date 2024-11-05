2ffac13d59d1605723068adb38b68b75e14ca50f
zhimin.zeng <zhimin.zeng@bambulab.com>
2024-11-05 20:14:57 +0800

    ENH: support auto pa cali for multi_extruder printer
    jira: none
    Change-Id: I835a0e20de81f9af7c40983e00bdb37ea6c95a68

--- src/slic3r/Utils/CalibUtils.cpp
+++ src/slic3r/Utils/CalibUtils.cpp
@@ -55,6 +55,16 @@ void get_default_k_n_value(const std::string &filament_id, float &k, float &n)
     }
 }
 
+wxString get_nozzle_volume_type_name(NozzleVolumeType type)
+{
+    if (NozzleVolumeType::nvtNormal == type) {
+        return _L("Normal");
+    } else if (NozzleVolumeType::nvtBigTraffic == type) {
+        return _L("BigTraffic");
+    }
+    return wxString();
+}
+
 std::string get_calib_mode_name(CalibMode cali_mode, int stage)
 {
     switch(cali_mode) {
