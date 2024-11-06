3921adb3421ef745e32e933a47ecef7e7dd1f7c7
qing.zhang <qing.zhang@bambulab.com>
2024-11-06 18:14:32 +0800

    ENH: update plate text and filament printable warning
    jira: none
    
    Signed-off-by: qing.zhang <qing.zhang@bambulab.com>
    Change-Id: Id9ec632b6d663a5060e63bf151fc277411cced0a

--- src/slic3r/GUI/GLCanvas3D.cpp
+++ src/slic3r/GUI/GLCanvas3D.cpp
@@ -9534,7 +9534,7 @@ void GLCanvas3D::_set_warning_notification(EWarning warning, bool state)
             warning += std::to_string(filament + 1);
             warning+=" ";
         }
-        text  = (boost::format(_u8L("filaments %s cannot be printed directly on the surface of %s")) % warning % m_gcode_viewer.filament_printable_reuslt.plate_name).str();
+        text  = (boost::format(_u8L("filaments %s cannot be printed directly on the surface of this plate")) % warning ).str();
         error = ErrorType::SLICING_ERROR;
         break;
     }
