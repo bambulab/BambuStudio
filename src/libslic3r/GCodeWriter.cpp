0a085c1db8ab2820687bf05237444b6fe11cdf68
xun.zhang <xun.zhang@bambulab.com>
2024-12-10 16:19:27 +0800

    FIX: get wrong value in retract params
    1.Should get value by filament id instead of extruder id
    2.Fix many other issues caused by incorrect usage
    
    jira:NONE
    
    Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
    Change-Id: I3278d7de0f8976a97c9e5ccef80bba4a58553f5a
    (cherry picked from commit 30c51dd1171fc16ba778308745fab2eb246aedd0)

--- src/libslic3r/GCodeWriter.cpp
+++ src/libslic3r/GCodeWriter.cpp
@@ -692,10 +692,11 @@ std::string GCodeWriter::lift(LiftType lift_type, bool spiral_vase)
     {
         //BBS
         int extruder_id = filament()->extruder_id();
+        int filament_id = filament()->id();
         double above = this->config.retract_lift_above.get_at(extruder_id);
         double below = this->config.retract_lift_below.get_at(extruder_id);
         if (m_pos.z() >= above && m_pos.z() <= below)
-            target_lift = this->config.z_hop.get_at(extruder_id);
+            target_lift = this->config.z_hop.get_at(filament_id);
     }
     // BBS
     if (m_lifted == 0 && m_to_lift == 0 && target_lift > 0) {
