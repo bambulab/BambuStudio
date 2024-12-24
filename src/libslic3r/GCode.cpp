6eaff77f4efa18b8e417ad4dabdb5ab28fdc159e
qing.zhang <qing.zhang@bambulab.com>
2024-12-24 15:30:53 +0800

    ENH: add smoothing z dir on slicing process bar
    Jira: none
    
    Signed-off-by: qing.zhang <qing.zhang@bambulab.com>
    Change-Id: I0bab84ee31864917142f9bcb407f957f3a2ea3ce

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2671,7 +2671,9 @@ void GCode::process_layers(
         tbb::parallel_pipeline(12, generator & parsing & cooling & write_gocde & output);
     else {
         tbb::parallel_pipeline(12, generator & parsing & cooling & build_node);
-
+        std::string message;
+        message = L("Smoothing z direction speed");
+        m_print->set_status(85, message);
         //append data
         for (const LayerResult &res : layers_results) {
             //remove empty gcode layer caused by support independent layers
@@ -2682,7 +2684,8 @@ void GCode::process_layers(
         }
 
         smooth_calculator.smooth_layer_speed();
-
+        message = L("Exporting G-code");
+        m_print->set_status(90, message);
         tbb::parallel_pipeline(12, calculate_layer_time & write_gocde & output);
     }
 }
