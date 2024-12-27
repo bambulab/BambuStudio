1806c09b827edc6739f60b5c4212fc72d1ca9878
Stone Li <stone.li@bambulab.com>
2024-12-27 16:18:24 +0800

    ENH: send G28 X when in printing
    JIRA: STUD-9349
    
    Change-Id: Ib14af76830380036fadcae707438472bed25d4aa
    Signed-off-by: Stone Li <stone.li@bambulab.com>

--- src/slic3r/GUI/DeviceManager.cpp
+++ src/slic3r/GUI/DeviceManager.cpp
@@ -1670,7 +1670,12 @@ int MachineObject::command_auto_leveling()
 
 int MachineObject::command_go_home()
 {
-    return this->publish_gcode("G28 \n");
+    if (this->is_in_printing()) {
+        return this->publish_gcode("G28 X\n");
+    }
+    else {
+        return this->publish_gcode("G28 \n");
+    }
 }
 
 int MachineObject::command_control_fan(FanType fan_type, bool on_off)
