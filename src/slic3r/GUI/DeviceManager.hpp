73b457ae74d4a8106444f9220b0d29ef9e12ff43
xin.zhang <xin.zhang@bambulab.com>
2025-01-14 09:56:38 +0800

    FIX: Can do switch while printing pause
    jira: [STUDIO-9789]
    Change-Id: I8d5d2195adcd2cd048d95303f0562f24b696e34e

--- src/slic3r/GUI/DeviceManager.hpp
+++ src/slic3r/GUI/DeviceManager.hpp
@@ -1165,6 +1165,7 @@ public:
     bool can_pause();
     bool can_abort();
     bool is_in_printing();
+    bool is_in_printing_pause() const;
     bool is_in_prepare();
     bool is_printing_finished();
     bool is_core_xy();
