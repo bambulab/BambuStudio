91452df326e80cb3d15f7680fe91bdd8d128c0d3
zhou.xu <zhou.xu@bambulab.com>
2025-01-19 10:32:40 +0800

    FIX:fix crash for "close wxframe at System Bar"
    jira: STUDIO-9992
    Change-Id: I8cdfff70394880a2dc04f2fa2e37ad6a11eba5a9

--- src/slic3r/GUI/SyncAmsInfoDialog.cpp
+++ src/slic3r/GUI/SyncAmsInfoDialog.cpp
@@ -4415,6 +4415,7 @@ SyncNozzleAndAmsDialog::SyncNozzleAndAmsDialog(wxWindow *parent, InputInfo &inpu
 
     m_sizer_main->Add(bSizer_button, 1, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, FromDIP(20));
 
+    Bind(wxEVT_CLOSE_WINDOW, [this](auto &e) { this->on_hide(); });
     SetSizer(m_sizer_main);
     Layout();
     Fit();
@@ -4454,7 +4455,7 @@ FinishSyncAmsDialog::FinishSyncAmsDialog(wxWindow *parent, InputInfo &input_info
     , m_input_info(input_info)
 {
     // SetBackgroundStyle(wxBackgroundStyle::wxBG_STYLE_TRANSPARENT);
-    SetTransparent(200);
+    SetTransparent(220);
     SetBackgroundColour(wxColour(23, 25, 22, 128));
     auto win_width = 288;
     SetMinSize(wxSize(FromDIP(win_width), -1));
@@ -4513,6 +4514,7 @@ FinishSyncAmsDialog::FinishSyncAmsDialog(wxWindow *parent, InputInfo &input_info
     //bSizer_button->Add(m_button_cancel, 0, wxALIGN_RIGHT | wxLEFT | wxTOP, FromDIP(10));
     m_sizer_main->Add(bSizer_button, 1, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, FromDIP(20));
 
+    Bind(wxEVT_CLOSE_WINDOW, [this](auto &e) { this->on_hide(); });
     SetSizer(m_sizer_main);
     Layout();
     Fit();
