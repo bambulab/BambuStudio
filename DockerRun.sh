dad98940883650bd56786ab525f10ad404d0fdd1
PatrickChenHZ <2540932806@qq.com>
2025-01-23 11:53:01 -0600

    update DockeRun to match with lastest run suggestions

--- DockerRun.sh
+++ DockerRun.sh
@@ -9,25 +9,20 @@ set -x
 #  -v $HOME/.Xauthority:/root/.Xauthority \
 #  You also need to run "xhost +" on your host system
 # Bambu Studio also require the parent directory for the configuration directory to be present to start
-#  which means it is important to make sure user is passed to container correctly
-#  if the following configuration does not work with error: "boost::filesystem::create_directory: No such file or directory"
-#  try replacing -u line with 
-#  -u $(id -u ${USER}):$(id -g ${USER}) \
-#  and add 
-#  -e HOME=/home/$USER \
+#  to prevent your local machines's bambu studio config passed to docker container when you map your home directory, add:
+# -v :SHOME/.config/BambuStudio
+set -x
 docker run \
   `# Use the hosts networking.  Printer wifi and also dbus communication` \
   --net=host \
   `# Some X installs will not have permissions to talk to sockets for shared memory` \
   --ipc host \
-  `# Run as your workstations username to keep permissions the same` \
-  -u $USER \
   `# Bind mount your home directory into the container for loading/saving files` \
   -v $HOME:$HOME \
+  `# Pass some X Auth file to allow x11 to connect to your host x instance` \
   -v $HOME/.Xauthority:/tmp/.Xauthority \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e XAUTHORITY=/tmp/.Xauthority \
-  -u $(id -u ${USER}):$(id -g ${USER}) \
   `# Pass the X display number to the container` \
   -e DISPLAY=$DISPLAY \
   `# It seems that libGL and dbus things need privileged mode` \
@@ -37,5 +32,4 @@ docker run \
   `# Remove container when it is finished` \
   --rm \
   `# Pass all parameters from this script to the bambu ENTRYPOINT binary` \
-  bambustudio $* 
-  
+  bambu_studio_yes_args $* 
