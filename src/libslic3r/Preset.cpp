89b59f1c41e0f360457622438a09237bfa1eaa18
jiangkai.zhao <jiangkai.zhao@bambulab.com>
2025-02-08 10:04:41 +0800

    ENH: add wipe_tower infll gap
    and fix wipe error when enable timelapse and in single color
    and set rib wall as default
    jira: none
    
    Change-Id: Ic365bb7ee0ee6715c9d4f4f00b4bca9fd472c61a

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -880,7 +880,7 @@ static std::vector<std::string> s_Preset_print_options {
     "top_surface_line_width", "support_line_width", "infill_wall_overlap", "bridge_flow",
     "elefant_foot_compensation", "xy_contour_compensation", "xy_hole_compensation", "resolution", "enable_prime_tower",
     "prime_tower_width", "prime_tower_brim_width", "prime_tower_skip_points",
-    "prime_tower_rib_wall","prime_tower_extra_rib_length","prime_tower_rib_width","prime_tower_fillet_wall",
+    "prime_tower_rib_wall","prime_tower_extra_rib_length","prime_tower_rib_width","prime_tower_fillet_wall","prime_tower_infill_gap",
     "enable_circle_compensation", "circle_compensation_speed", "circle_compensation_manual_offset", "apply_scarf_seam_on_circles",
     "counter_coef_1", "counter_coef_2", "counter_coef_3", "hole_coef_1", "hole_coef_2", "hole_coef_3",
     "counter_limit_min", "counter_limit_max", "hole_limit_min", "hole_limit_max", "diameter_limit",
