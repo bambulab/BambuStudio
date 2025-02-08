ed1e9678c351dc037f2e3cc7ea0e19d46731699c
xin.zhang <xin.zhang@bambulab.com>
2025-02-08 14:47:28 +0800

    FIX: try to reduce page fault while idling
    jira: [none]
    Change-Id: I563cbf4a8ba8a4ae1e5b1b1f735753d3aef23e16

--- src/slic3r/GUI/Widgets/AMSControl.cpp
+++ src/slic3r/GUI/Widgets/AMSControl.cpp
@@ -3075,6 +3075,9 @@ void AMSControl::CreateAmsDoubleNozzle()
     m_down_road->UpdatePassRoad(AMSPanelPos::RIGHT_PANEL, -1, AMSPassRoadSTEP::AMS_ROAD_STEP_NONE);
     m_extruder->OnAmsLoading(false, MAIN_NOZZLE_ID);
 
+    m_amswin->Layout();
+    m_amswin->Fit();
+
     //Thaw();
 }
 
@@ -3160,6 +3163,9 @@ void AMSControl::CreateAmsSingleNozzle()
     m_extruder->updateNozzleNum(1);
     m_extruder->OnAmsLoading(false, MAIN_NOZZLE_ID);
 
+    m_amswin->Layout();
+    m_amswin->Fit();
+
     //Refresh();
     //Thaw();
 }
@@ -3198,7 +3204,12 @@ void AMSControl::show_noams_mode()
 
 void AMSControl::show_auto_refill(bool show)
 {
-    //m_img_ams_backup->Show(show);
+    if (m_button_auto_refill->IsShown() == show)
+    {
+        return;
+    }
+
+    m_button_auto_refill->Show(show);
     m_amswin->Layout();
     m_amswin->Fit();
 }
