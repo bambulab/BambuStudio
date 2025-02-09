125ed81401125c97f49f6cb3694ab533c31a7178
gerrit <gerrit@bambulab.com>
2025-02-09 10:12:11 +0800

    ci: update network module based on commit 9104f26
    Change-Id: Ideb45bb05d92df6057a32728a498be8896bf02c1

--- src/slic3r/Utils/bambu_networking.hpp
+++ src/slic3r/Utils/bambu_networking.hpp
@@ -97,7 +97,7 @@ namespace BBL {
 #define BAMBU_NETWORK_LIBRARY               "bambu_networking"
 #define BAMBU_NETWORK_AGENT_NAME            "bambu_network_agent"
 
-#define BAMBU_NETWORK_AGENT_VERSION         "01.10.02.19"
+#define BAMBU_NETWORK_AGENT_VERSION         "01.10.02.20"
 
 //iot preset type strings
 #define IOT_PRINTER_TYPE_STRING     "printer"
