e684f8fcc67464f852f61cb5b7389dfd3491982b
wintergua <wintergua.cui@bambulab.com>
2025-02-19 15:32:58 +0800

    FIX: wipe tpwer should be no_brim_area
    jira-id: none
    Change-Id: Ia8b9c1639a62d90421932594b3709eba71b5e723
    (cherry picked from commit 68eb40bc74d75fece451ed3b4b4b767b46c238b3)

--- src/libslic3r/Print.cpp
+++ src/libslic3r/Print.cpp
@@ -1874,6 +1874,11 @@ void Print::process(std::unordered_map<std::string, long long>* slice_time, bool
         }
         this->set_done(psWipeTower);
     }
+
+    if (this->has_wipe_tower()) {
+        m_fake_wipe_tower.set_pos({ m_config.wipe_tower_x.get_at(m_plate_index), m_config.wipe_tower_y.get_at(m_plate_index) });
+    }
+
     if (this->set_started(psSkirtBrim)) {
         this->set_status(70, L("Generating skirt & brim"));
 
