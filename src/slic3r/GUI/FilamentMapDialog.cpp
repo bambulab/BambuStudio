7bcc83682e314ea12c5c5229f394015d61cff345
xun.zhang <xun.zhang@bambulab.com>
2025-03-12 11:01:08 +0800

    FIX: dark mode display in dialog
    jira:STUDIO-10811
    
    Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
    Change-Id: I174541d97efe8d0beea4913d23375f82b104b7a2

--- src/slic3r/GUI/FilamentMapDialog.cpp
+++ src/slic3r/GUI/FilamentMapDialog.cpp
@@ -246,7 +246,7 @@ FilamentMapDialog::FilamentMapDialog(wxWindow                       *parent,
     Fit();
 
     CenterOnParent();
-    GUI::wxGetApp().UpdateDarkUIWin(this);
+    wxGetApp().UpdateDlgDarkUI(this);
 }
 
 FilamentMapMode FilamentMapDialog::get_mode()
