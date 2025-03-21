61f61bce3ed12fbceff7fe98eead6b27a39df5ea
zhou.xu <zhou.xu@bambulab.com>
2025-03-21 17:29:37 +0800

    ENH:modidy width of SyncNozzleAndAmsDialog and FinishSyncAmsDialog
    jira: STUDIO-11045
    Change-Id: I99164b2172aeb7fb955348fc87f7da24aefe930c

--- src/slic3r/GUI/SyncAmsInfoDialog.cpp
+++ src/slic3r/GUI/SyncAmsInfoDialog.cpp
@@ -3533,7 +3533,7 @@ std::string SyncAmsInfoDialog::get_print_status_info(PrintDialogStatus status)
 
 SyncNozzleAndAmsDialog::SyncNozzleAndAmsDialog(InputInfo &input_info)
     : BaseTransparentDPIFrame(static_cast<wxWindow *>(wxGetApp().mainframe),
-                              300,
+                              320,
                               input_info.dialog_pos,
                               90,
                               _L("Successfully synchronized nozzle and AMS number information."),
@@ -3566,7 +3566,7 @@ void SyncNozzleAndAmsDialog::update_info(InputInfo &info) {
 
 FinishSyncAmsDialog::FinishSyncAmsDialog(InputInfo &input_info)
     : BaseTransparentDPIFrame(static_cast<wxWindow *>(wxGetApp().mainframe),
-                              300,
+                              310,
                               input_info.dialog_pos,
                               68,
                               _L("Successfully synchronized color and type of filament from printer."),
