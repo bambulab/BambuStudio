931e1a8fb81f3d263c6e23fa3fb971d5c83d2713
zorro.zhang <zorro.zhang@bambulab.com>
2025-04-15 12:12:09 +0800

    ENH: Reduce Policy Choose Callback
    JIRA: none
    Change-Id: Id3f9cb79851fb2b9d21f1dc2b423da21d175b39f

--- resources/web/guide/3/3.js
+++ resources/web/guide/3/3.js
@@ -5,29 +5,27 @@ function OnInit()
 {
 	TranslatePage();
 	
-	SendPrivacySelect();
+	//SendPrivacySelect();
 	
 	let strRegion=GetQueryString('region');
 	if( strRegion!=null )
 		m_Region=strRegion.toLowerCase();
 }
 
-
-function SendPrivacySelect()
-{	
-	let nVal="refuse";
-	if( $('#ChoosePrivacy').is(':checked') ) 
-		nVal="agree";
-	
-	var tSend={};
-	tSend['sequence_id']=Math.round(new Date() / 1000);
-	tSend['command']="user_private_choice";
-	tSend['data']={};
-	tSend['data']['action']=nVal;
-	
-	SendWXMessage( JSON.stringify(tSend) );	
-}
-
+//function SendPrivacySelect()
+//{	
+//	let nVal="refuse";
+//	if( $('#ChoosePrivacy').is(':checked') ) 
+//		nVal="agree";
+//	
+//	var tSend={};
+//	tSend['sequence_id']=Math.round(new Date() / 1000);
+//	tSend['command']="user_private_choice";
+//	tSend['data']={};
+//	tSend['data']['action']=nVal;
+//	
+//	SendWXMessage( JSON.stringify(tSend) );	
+//}
 
 function SendPrivacyValue( strVal )
 {
