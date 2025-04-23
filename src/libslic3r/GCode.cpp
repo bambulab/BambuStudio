3f5fd81de94446efbda1e43c9ee0b2dc7e582e98
xun.zhang <xun.zhang@bambulab.com>
2025-04-23 17:11:24 +0800

    ENH: add filament map and filament list in GCode head
    jira: NONE
    
    Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
    Change-Id: Id214bcf5f76e6c59d4b8bcb55583f96b347630bf

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -1991,6 +1991,18 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
         file.writeln(max_height_z_tip.str());
     }
 
+    {
+        auto used_filaments = print.get_slice_used_filaments(false);
+        std::ostringstream out;
+        out << "; filament: ";
+        for (size_t idx = 0; idx < used_filaments.size(); ++idx) {
+            if (idx != 0)
+                out << ',';
+            out << used_filaments[idx] + 1;
+        }
+        file.writeln(out.str());
+    }
+
     file.write_format("; HEADER_BLOCK_END\n\n");
 
     //BBS: write global config at the beginning of gcode file because printer need these config information
