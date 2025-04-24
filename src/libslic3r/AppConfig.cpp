1142a4d3f68451cecd362b0cbf073f74d38e8842
zhou.xu <zhou.xu@bambulab.com>
2025-04-24 09:55:11 +0800

    NEW:Make sidebar collapsible, resizable
    jira:STUDIO-11623
    Code is from OrcaSlicer,thanks for OrcaSlicer and Noisyfox
    commit 7cfa4f3bcd793d4c394134fc5665fa3732ee1c82
    Author: Noisyfox <timemanager.rick@gmail.com>
    Date:   Fri Dec 8 17:16:16 2023 +0800
        QoL: Make sidebar collapsible, resizable, movable, floatable and dockable (#2972)
    
    Change-Id: I7f2b16f87ab1affe6719f9b348680f392cf245f0

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -176,6 +176,8 @@ void AppConfig::set_defaults()
         set_bool("enable_merge_color_by_sync_ams", false);
     if (get("ams_sync_match_full_use_color_dist").empty())
         set_bool("ams_sync_match_full_use_color_dist", false);
+    if (get("enable_sidebar_resizable").empty())
+        set_bool("enable_sidebar_resizable", false);
 
     if (get("zoom_to_mouse").empty())
         set_bool("zoom_to_mouse", false);
