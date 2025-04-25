e13c98e4ad5f168cfd058709eed20765c3d42304
xun.zhang <xun.zhang@bambulab.com>
2025-04-25 11:32:25 +0800

    ENH: new overhang alogrithm for arachne
    1. Speed up the wall generate
    
    jira:STUDIO-11630
    
    Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
    Change-Id: Ibb4129b9530095296e5e74412a44aeb62d3497e5

---
+++ src/libslic3r/OverhangDetector.hpp
@@ -0,0 +1,40 @@
+#ifndef OVERHANG_DETECTOR_HPP
+#define OVERHANG_DETECTOR_HPP
+
+#include "AABBTreeIndirect.hpp"
+#include "ExtrusionEntityCollection.hpp"
+#include "ClipperUtils.hpp"
+#include "Flow.hpp"
+
+using ZPoint = ClipperLib_Z::IntPoint;
+using ZPath = ClipperLib_Z::Path;
+using ZPaths = ClipperLib_Z::Paths;
+
+static const int overhang_sampling_number = 6;
+static const double min_degree_gap = 0.1;
+static const int max_overhang_degree = overhang_sampling_number - 1;
+static const std::vector<double> non_uniform_degree_map = { 0, 10, 25, 50, 75, 100};
+static const int insert_point_count = 3;
+
+namespace Slic3r {
+
+    class OverhangDistancer
+    {
+        std::vector<Linef>                lines;
+        AABBTreeIndirect::Tree<2, double> tree;
+
+    public:
+        OverhangDistancer(const Polygons layer_polygons);
+        float distance_from_perimeter(const Vec2f& point) const;
+    };
+
+
+    ZPaths clip_extrusion(const ZPath& subject, const ZPaths& clip, ClipperLib_Z::ClipType clipType);
+
+    ZPath add_sampling_points(const ZPath& path, double min_sampling_interval);
+
+    ExtrusionPaths detect_overhang_degree(const Flow& flow, const ExtrusionRole role, const Polygons& lower_polys, const ClipperLib_Z::Paths& clip_paths, const ClipperLib_Z::Path& extrusion_paths, const double nozzle_diameter);
+
+}
+
+#endif
