a02a68d6b4bd29bf42c4d4a985bbc33052f2895c
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2025-05-15 10:57:14 -0400

    Patch GMP to build on GCC15
    Co-authored-by: Bastien Nocera <hadess@hadess.net>

--- deps/GMP/GMP.cmake
+++ deps/GMP/GMP.cmake
@@ -25,7 +25,7 @@ if (MSVC)
         COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/lib/${_gmplib} ${_dstdir}/lib/
         COMMAND ${CMAKE_COMMAND} -E copy ${_srcdir}/lib/${_gmpdll} ${_dstdir}/bin/
     )
-    
+
     add_custom_target(dep_GMP SOURCES ${_output})
 
 else ()
@@ -72,7 +72,8 @@ else ()
         URL https://github.com/bambulab/gmp/archive/refs/tags/6.2.1.tar.gz
         URL_HASH SHA256=705ae57ee2014b2c6fc0f572c85ee43276b99b6b256ee16c1a9d3a8c4e3609d5
         DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/GMP
-        BUILD_IN_SOURCE ON 
+        PATCH_COMMAND git apply --verbose ${CMAKE_CURRENT_LIST_DIR}/0001-GMP_GCC15.patch
+        BUILD_IN_SOURCE ON
         CONFIGURE_COMMAND  env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --enable-shared=no --enable-cxx=yes --enable-static=yes "--prefix=${DESTDIR}/usr/local" ${_gmp_build_tgt}
         BUILD_COMMAND     make -j
         INSTALL_COMMAND   make install
