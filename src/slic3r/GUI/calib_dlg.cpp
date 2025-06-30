dc4acfaef1a3a932e0e58fb7983e2d82e2a8871c
zhimin.zeng <zhimin.zeng@bambulab.com>
2025-06-30 09:57:27 +0800

    FIX: fix some calib dialog bug
    jira: STUDIO-12984 & STUDIO-12979 & STUDIO-12978
    Change-Id: I29fadd20b29c76a288755122dd027820fed9252e

--- src/slic3r/GUI/calib_dlg.cpp
+++ src/slic3r/GUI/calib_dlg.cpp
@@ -9,7 +9,7 @@
 #include "libslic3r/PrintConfig.hpp"
 
 namespace Slic3r { namespace GUI {
-    
+
 wxBoxSizer* create_item_checkbox(wxString title, wxWindow* parent, bool* value, CheckBox*& checkbox)
 {
     wxBoxSizer* m_sizer_checkbox = new wxBoxSizer(wxHORIZONTAL);
@@ -185,7 +185,7 @@ void PA_Calibration_Dlg::reset_params() {
 
     if (!isDDE) {
         m_tiEndPA->GetTextCtrl()->SetValue(wxString::FromDouble(1.0));
-        
+
         if (m_params.mode == CalibMode::Calib_PA_Pattern) {
             m_tiPAStep->GetTextCtrl()->SetValue(wxString::FromDouble(0.05));
         } else {
@@ -194,12 +194,12 @@ void PA_Calibration_Dlg::reset_params() {
     }
 }
 
-void PA_Calibration_Dlg::on_start(wxCommandEvent& event) { 
+void PA_Calibration_Dlg::on_start(wxCommandEvent& event) {
     bool read_double = false;
     read_double = m_tiStartPA->GetTextCtrl()->GetValue().ToDouble(&m_params.start);
     read_double = read_double && m_tiEndPA->GetTextCtrl()->GetValue().ToDouble(&m_params.end);
     read_double = read_double && m_tiPAStep->GetTextCtrl()->GetValue().ToDouble(&m_params.step);
-    if (!read_double || m_params.start < 0 || m_params.step < EPSILON || m_params.end < m_params.start + m_params.step) {
+    if (!read_double || m_params.start < 0 || m_params.step < 10 * EPSILON || m_params.end < m_params.start + m_params.step) {
         MessageDialog msg_dlg(nullptr, _L("Please input valid values:\nStart PA: >= 0.0\nEnd PA: > Start PA\nPA step: >= 0.001)"), wxEmptyString, wxICON_WARNING | wxOK);
         msg_dlg.ShowModal();
         return;
@@ -222,17 +222,17 @@ void PA_Calibration_Dlg::on_start(wxCommandEvent& event) {
     EndModal(wxID_OK);
 
 }
-void PA_Calibration_Dlg::on_extruder_type_changed(wxCommandEvent& event) { 
+void PA_Calibration_Dlg::on_extruder_type_changed(wxCommandEvent& event) {
     PA_Calibration_Dlg::reset_params();
-    event.Skip(); 
+    event.Skip();
 }
-void PA_Calibration_Dlg::on_method_changed(wxCommandEvent& event) { 
+void PA_Calibration_Dlg::on_method_changed(wxCommandEvent& event) {
     PA_Calibration_Dlg::reset_params();
-    event.Skip(); 
+    event.Skip();
 }
 
 void PA_Calibration_Dlg::on_dpi_changed(const wxRect& suggested_rect) {
-    this->Refresh(); 
+    this->Refresh();
     Fit();
 }
 
@@ -247,7 +247,9 @@ enum FILAMENT_TYPE : int
     tPLA = 0,
     tABS_ASA,
     tPETG,
+    tPCTG,
     tTPU,
+    tTPU_AMS,
     tPA_CF,
     tPET_CF,
     tCustom
@@ -363,7 +365,7 @@ Temp_Calibration_Dlg::Temp_Calibration_Dlg(wxWindow* parent, wxWindowID id, Plat
         e.Skip();
         });
 
-    
+
 }
 
 Temp_Calibration_Dlg::~Temp_Calibration_Dlg() {
@@ -379,7 +381,7 @@ void Temp_Calibration_Dlg::on_start(wxCommandEvent& event) {
     read_long = read_long && m_tiEnd->GetTextCtrl()->GetValue().ToULong(&end);
 
     if (!read_long || start > 350 || end < 180  || end > (start - 5)) {
-        MessageDialog msg_dlg(nullptr, _L("Please input valid values:\nStart temp: <= 350\nEnd temp: >= 180\nStart temp > End temp + 5)"), wxEmptyString, wxICON_WARNING | wxOK);
+        MessageDialog msg_dlg(nullptr, _L("Please input valid values:\nStart temp: <= 350\nEnd temp: >= 180\nStart temp >= End temp + 5)"), wxEmptyString, wxICON_WARNING | wxOK);
         msg_dlg.ShowModal();
         return;
     }
@@ -404,7 +406,12 @@ void Temp_Calibration_Dlg::on_filament_type_changed(wxCommandEvent& event) {
             start = 250;
             end = 230;
             break;
+        case tPCTG:
+            start = 280;
+            end   = 240;
+            break;
         case tTPU:
+        case tTPU_AMS:
             start = 240;
             end = 210;
             break;
@@ -422,7 +429,7 @@ void Temp_Calibration_Dlg::on_filament_type_changed(wxCommandEvent& event) {
             end = 190;
             break;
     }
-    
+
     m_tiEnd->GetTextCtrl()->SetValue(std::to_string(end));
     m_tiStart->GetTextCtrl()->SetValue(std::to_string(start));
     event.Skip();
