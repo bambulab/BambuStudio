b1631bdf1fd034138b5e6bec2c1bde32f2aacefa
weizhen.xie <weizhen.xie@bambulab.com>
2025-07-01 20:25:19 +0800

    ENH：Object skipping supports skipping flushing of filament change
    Jira：[STUDIO-12781]
    
    Change-Id: Ia21d07b0ef107867cce55631d99bd03caebd6387

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -1034,7 +1034,7 @@ static std::vector<std::string> s_Preset_printer_options {
     "printhost_cafile","printhost_port","printhost_authorization_type",
     "printhost_user", "printhost_password", "printhost_ssl_ignore_revoke",
     "use_relative_e_distances", "extruder_type","use_firmware_retraction",
-    "grab_length","machine_switch_extruder_time","hotend_cooling_rate","hotend_heating_rate","enable_pre_heating", "physical_extruder_map",
+    "grab_length","machine_switch_extruder_time","hotend_cooling_rate","hotend_heating_rate","enable_pre_heating", "support_object_skip_flush","physical_extruder_map",
     "bed_temperature_formula","machine_prepare_compensation_time", "nozzle_flush_dataset","apply_top_surface_compensation"
 };
 
