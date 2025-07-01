1a9466018e62dfa7b7be611ea30673c8cc0864f2
qing.zhang <qing.zhang@bambulab.com>
2025-07-01 10:01:03 +0800

    FIX:Auxiliary part cooling fan option not work
    Github: 6774
    
    Signed-off-by: qing.zhang <qing.zhang@bambulab.com>

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2252,7 +2252,7 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
     this->print_machine_envelope(file, print, initial_extruder_id);
 
     // Disable fan.
-    if (print.config().close_fan_the_first_x_layers.get_at(initial_extruder_id)) {
+    if (m_config.auxiliary_fan.value  && print.config().close_fan_the_first_x_layers.get_at(initial_extruder_id)) {
         file.write(m_writer.set_fan(0));
         //BBS: disable additional fan
         file.write(m_writer.set_additional_fan(0));
@@ -2787,7 +2787,8 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
 
     file.write(m_writer.set_fan(0));
     //BBS: make sure the additional fan is closed when end
-    file.write(m_writer.set_additional_fan(0));
+    if (m_config.auxiliary_fan.value)
+        file.write(m_writer.set_additional_fan(0));
     //BBS: close spaghetti detector
     //Note: M981 is also used to tell xcam the last layer is finished, so we need always send it even if spaghetti option is disabled.
     //if (print.config().spaghetti_detector.value)
