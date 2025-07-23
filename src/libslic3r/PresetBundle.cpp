cb470ae21279a555c7f4718673d83e3818b46bf0
maosheng.wei <maosheng.wei@bambulab.com>
2025-07-23 15:15:27 +0800

    FIX: Update the alias of imported presets
    Jira: STUDIO-12801
    
    Change-Id: I7583a2022a0f565103953ee0ca742593769da81f

--- src/libslic3r/PresetBundle.cpp
+++ src/libslic3r/PresetBundle.cpp
@@ -883,7 +883,7 @@ bool PresetBundle::import_json_presets(PresetsConfigSubstitutions &            s
                                      << ", which were removed";
         if (!config_substitutions.empty())
             substitutions.push_back({name, collection->type(), PresetConfigSubstitutions::Source::UserFile, file, std::move(config_substitutions)});
-
+        collection->set_custom_preset_alias(preset);
         preset.save(inherit_preset ? &inherit_preset->config : nullptr);
         result.push_back(file);
     } catch (const std::ifstream::failure &err) {
