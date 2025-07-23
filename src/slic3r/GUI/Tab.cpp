ee733bd670a8d83a95dddfde500ab5382e1fdaf2
chunmao.guo <chunmao.guo@bambulab.com>
2025-07-23 19:12:12 +0800

    FIX: TabPrinter toggle_option with variant_index
    Change-Id: I9d3a2f2d4707add057c881d20830f9fc1b4a4a89
    Jira: STUDIO-13499

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -4710,7 +4710,7 @@ void TabPrinter::toggle_options()
             //BBS
             toggle_option(el, retraction && !use_firmware_retraction, i);
 
-        bool wipe = retraction && m_config->opt_bool_nullable("wipe", i);
+        bool wipe = retraction && m_config->opt_bool_nullable("wipe", variant_index);
         toggle_option("retract_before_wipe", wipe, i);
 
         if (use_firmware_retraction && wipe) {
