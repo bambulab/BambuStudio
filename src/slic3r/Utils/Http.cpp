bd3d5fd263e704c498c028ec01eef04b5335e441
hemai <yuliang.zhang@bambulab.com>
2025-07-28 17:18:16 +0800

    FIX: third-party printers send print crash
    Jira: [STUDIO-13681]
    Change-Id: I77892f79d62597a4367b4bdf5bae0bd90d2ceca2

--- src/slic3r/Utils/Http.cpp
+++ src/slic3r/Utils/Http.cpp
@@ -110,7 +110,6 @@ struct Http::priv
     std::string headers;
 	size_t limit;
 	bool cancel;
-    std::unique_ptr<fs::ifstream> putFile;
 
 	std::thread io_thread;
 	Http::CompleteFn completefn;
@@ -254,10 +253,8 @@ int Http::priv::xfercb_legacy(void *userp, double dltotal, double dlnow, double 
 size_t Http::priv::form_file_read_cb(char *buffer, size_t size, size_t nitems, void *userp)
 {
 	try {
-		auto putFile = reinterpret_cast<std::unique_ptr<fs::ifstream>*>(userp);
-		if (!putFile) { throw std::runtime_error(std::string("The unique_ptr is nullptr! please check"));  return CURL_READFUNC_ABORT; }
+        auto fstream = static_cast<fs::ifstream *>(userp);
 
-		fs::ifstream* fstream = putFile->get();
 		if (!fstream) { throw std::runtime_error(std::string("The fstream is nullptr! please check")); return CURL_READFUNC_ABORT; }
 
 		fstream->read(buffer, size * nitems);
@@ -363,9 +360,10 @@ void Http::priv::set_put_body(const fs::path &path)
 	boost::system::error_code ec;
 	boost::uintmax_t filesize = file_size(path, ec);
 	if (!ec) {
-		putFile = std::make_unique<fs::ifstream>(path, std::ios_base::binary |std::ios_base::in);
+		form_files.emplace_back(path, std::ios_base::binary |std::ios_base::in);
+		auto &putFile = form_files.back();
 		::curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-		::curl_easy_setopt(curl, CURLOPT_READDATA, (void *) &putFile);
+		::curl_easy_setopt(curl, CURLOPT_READDATA, static_cast<void*>(&putFile));
 		::curl_easy_setopt(curl, CURLOPT_INFILESIZE, filesize);
 	}
 }
@@ -485,11 +483,6 @@ Http::Http(Http &&other) : p(std::move(other.p)) {}
 
 Http::~Http()
 {
-	if (p && p->putFile)
-	{
-		p->putFile.reset();
-	}
-
 	if (p && p->io_thread.joinable()) {
 		p->io_thread.detach();
 	}
