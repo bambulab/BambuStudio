cf287f68bf7c4b51a77381cd1d9858f828df0591
xin.zhang <xin.zhang@bambulab.com>
2025-08-06 10:33:02 +0800

    ENH: add new part: extinguish
    JIRA: [STUDIO-11578]
    Change-Id: I39dc15fe022f38d8c775c2675120bbbce05f95f3

--- src/slic3r/GUI/DeviceManager.cpp
+++ src/slic3r/GUI/DeviceManager.cpp
@@ -823,6 +823,7 @@ void MachineObject::clear_version_info()
     air_pump_version_info = DevFirmwareVersionInfo();
     laser_version_info = DevFirmwareVersionInfo();
     cutting_module_version_info = DevFirmwareVersionInfo();
+    extinguish_version_info = DevFirmwareVersionInfo();
     module_vers.clear();
 }
 
@@ -834,6 +835,8 @@ void MachineObject::store_version_info(const DevFirmwareVersionInfo& info)
         laser_version_info = info;
     } else if (info.isCuttingModule()) {
         cutting_module_version_info = info;
+    } else if (info.isExtinguishSystem()) {
+        extinguish_version_info = info;
     }
 
     module_vers.emplace(info.name, info);
