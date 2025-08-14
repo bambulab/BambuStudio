eaa6c4f61b509efe231a26733752efc6f325c240
qing.zhang <qing.zhang@bambulab.com>
2025-08-14 18:02:11 +0800

    ENH: Embeding wall to the infill
    Jira: 14316
    
    Embeding walls into the infill areas to ensure the strength of
    connection
    
    Signed-off-by: qing.zhang <qing.zhang@bambulab.com>
    Change-Id: If7ede3a6560ad9122aaf8d5278ae4305d8adce86

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -1012,6 +1012,7 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionFloat,                seam_slope_min_length))
     ((ConfigOptionInt,                  seam_slope_steps))
     ((ConfigOptionBool,                 seam_slope_inner_walls))
+    ((ConfigOptionBool,                 embedding_wall_into_infill))
 )
 
 PRINT_CONFIG_CLASS_DEFINE(
