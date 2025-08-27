d76da7254d5aaf9b96b70ea40b3f9fa8a55a01d8
weizhen.xie <weizhen.xie@bambulab.com>
2025-08-27 10:35:02 +0800

    FIX: During the “wave_seeds” stage of bridge generation, Clipper2 is used instead of Clipper to avoid incorrect handling of collinear intersections in Clipper.
    Jira: None
    Change-Id: I83ab9def1e54de2ddca89efccc53e8821330e4b4

--- src/clipper2/Clipper2Lib/src/clipper.engine.cpp
+++ src/clipper2/Clipper2Lib/src/clipper.engine.cpp
@@ -21,12 +21,16 @@
 #define nearbyint(a) _mm_cvtsd_si64(_mm_set_sd(a)) /* Note: expression type is (int64_t) */
 #endif
 
+#ifdef USINGZ
+namespace Clipper2Lib_Z {
+#else
 namespace Clipper2Lib {
+#endif
 
   static const Rect64 invalid_rect = Rect64(false);
 
-  // Every closed path (ie polygon) is made up of a series of vertices forming edge 
-  // 'bounds' that alternate between ascending bounds (containing edges going up 
+  // Every closed path (ie polygon) is made up of a series of vertices forming edge
+  // 'bounds' that alternate between ascending bounds (containing edges going up
   // relative to the Y-axis) and descending bounds. 'Local Minima' refers to
   // vertices where ascending and descending bounds join at the bottom, and
   // 'Local Maxima' are where ascending and descending bounds join at the top.
@@ -1801,14 +1805,14 @@ namespace Clipper2Lib {
 
       switch (fillrule_)
       {
-      case FillRule::Positive: 
-        if (edge_c->wind_cnt != 1) return; 
+      case FillRule::Positive:
+        if (edge_c->wind_cnt != 1) return;
         break;
-      case FillRule::Negative: 
-        if (edge_c->wind_cnt != -1) return; 
+      case FillRule::Negative:
+        if (edge_c->wind_cnt != -1) return;
         break;
-      default: 
-        if (std::abs(edge_c->wind_cnt) != 1) return; 
+      default:
+        if (std::abs(edge_c->wind_cnt) != 1) return;
       }
 
 #ifdef USINGZ
@@ -1929,7 +1933,7 @@ namespace Clipper2Lib {
     const bool e1_windcnt_in_01 = old_e1_windcnt == 0 || old_e1_windcnt == 1;
     const bool e2_windcnt_in_01 = old_e2_windcnt == 0 || old_e2_windcnt == 1;
 
-    if ((!IsHotEdge(e1) && !e1_windcnt_in_01) || 
+    if ((!IsHotEdge(e1) && !e1_windcnt_in_01) ||
       (!IsHotEdge(e2) && !e2_windcnt_in_01))
         return;
 
