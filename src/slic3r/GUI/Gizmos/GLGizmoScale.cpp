a5d102a5e4d1e9345313a26894dd6dacafdf2b89
zhou.xu <zhou.xu@bambulab.com>
2025-09-06 11:48:45 +0800

    FIX:Add protection before accessing boundingbox
    jira: STUDIO-14445
    Change-Id: I1ed7f8db8a7923513a8f2218fbe2c2906c194058

--- src/slic3r/GUI/Gizmos/GLGizmoScale.cpp
+++ src/slic3r/GUI/Gizmos/GLGizmoScale.cpp
@@ -331,7 +331,7 @@ bool GLGizmoScale3D::is_scalling_mode_locked() const
 void GLGizmoScale3D::on_render()
 {
     const Selection& selection = m_parent.get_selection();
-
+    if (selection.is_empty()) { return; }
     bool single_instance = selection.is_single_full_instance();
     bool single_volume = selection.is_single_modifier() || selection.is_single_volume();
 
