1513f47534d2492aa314f30473fce35dbfccb8a4
Mack <yongfang.bian@bambulab.com>
2025-09-13 21:08:10 +0800

    FIX:github workflow fix
    jira:nojira
    
    Change-Id: I8b1c20dbbf4c50a9d2c75c4a384ce7f157ee00f7

--- deps/CGAL/CGAL.cmake
+++ deps/CGAL/CGAL.cmake
@@ -1,7 +1,12 @@
+if (IN_GIT_REPO)
+    set(CGAL_DIRECTORY_FLAG --directory ${BINARY_DIR_REL}/dep_CGAL-prefix/src/dep_CGAL)
+endif ()
+
 bambustudio_add_cmake_project(
     CGAL
     URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.4.zip
     URL_HASH SHA256=d7605e0a5a5ca17da7547592f6f6e4a59430a0bc861948974254d0de43eab4c0
+    PATCH_COMMAND git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch
     DEPENDS ${BOOST_PKG} dep_GMP dep_MPFR
 )
 
