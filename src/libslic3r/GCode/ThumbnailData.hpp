82529a28465894ffc9ae15e2e043d53a478fea32
weizhen.xie <weizhen.xie@bambulab.com>
2025-09-16 10:08:44 +0800

    ENH: add initial layer time to plate_x.json and slice_info.config
    Jira: STUDIO-14504
    Change-Id: I32460f1c19cce7c729806e35370e8b4b57aa069e

--- src/libslic3r/GCode/ThumbnailData.hpp
+++ src/libslic3r/GCode/ThumbnailData.hpp
@@ -80,6 +80,7 @@ struct PlateBBoxData
     int first_extruder = 0;
     float nozzle_diameter = 0.4;
     std::string bed_type;
+    float first_layer_time;
     // version 1: use view type ColorPrint (filament color)
     // version 2: use view type FilamentId (filament id)
     int version = 2;
@@ -93,6 +94,7 @@ struct PlateBBoxData
         j["nozzle_diameter"] = nozzle_diameter;
         j["version"]         = version;
         j["bed_type"]        = bed_type;
+        j["first_layer_time"] = first_layer_time;
         for (const auto& bbox : bbox_objs) {
             nlohmann::json j_bbox;
             bbox.to_json(j_bbox);
