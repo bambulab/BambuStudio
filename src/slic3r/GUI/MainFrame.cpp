a6cc820b9727da721f66a51fdaef1b10c7451473
xun.zhang <xun.zhang@bambulab.com>
2025-09-26 16:44:59 +0800

    FIX: to much pop up if machine state is not synced
    jira:none
    
    Signed-off-by: xun.zhang <xun.zhang@bambulab.com>
    Change-Id: Ia1edf591b2c2720093c3a432dfc8c6949a56168a

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -1787,6 +1787,10 @@ wxBoxSizer* MainFrame::create_side_tools()
 
     m_slice_btn->Bind(wxEVT_BUTTON, [this](wxCommandEvent& event)
         {
+            m_plater->reset_check_status();
+            if (!m_plater->check_ams_status(m_slice_select == eSliceAll))
+                return;
+            m_plater->set_slice_from_slice_btn(true);
 
             //this->m_plater->select_view_3D("Preview");
             m_plater->exit_gizmo();
