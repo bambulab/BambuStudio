0dadece40c2038d24c949f87bc3fe53c6b031f32
jiaxi.chen <jiaxi.chen@bambulab.com>
2025-09-29 12:39:52 +0800

    Fix: expand interface to cover small area
    jira: STUDIO-14463
    Change-Id: Icc75f4db987be2da04fbbe31d3ffb45bb7ec7012

--- src/libslic3r/Support/TreeSupport.cpp
+++ src/libslic3r/Support/TreeSupport.cpp
@@ -2234,7 +2234,11 @@ void TreeSupport::draw_circles()
     bool on_buildplate_only = m_object_config->support_on_build_plate_only.value;
     Polygon branch_circle; //Pre-generate a circle with correct diameter so that we don't have to recompute those (co)sines every time.
 
-    if (print->config().top_z_overrides_xy_distance) m_ts_data->m_xy_distance = std::min(m_ts_data->m_xy_distance, double(top_z_distance));
+    double orig_xy_distance;
+    if (print->config().top_z_overrides_xy_distance) {
+        orig_xy_distance  = m_ts_data->m_xy_distance;
+        m_ts_data->m_xy_distance = std::min(m_ts_data->m_xy_distance, double(top_z_distance));
+    }
 
     // Use square support if there are too many nodes per layer because circle support needs much longer time to compute
     // Hower circle support can be printed faster, so we prefer circle for fewer nodes case.
@@ -2368,7 +2372,7 @@ void TreeSupport::draw_circles()
                             if (node.overhang.contour.size() > 100 || node.overhang.holes.size() > 1)
                                 area.emplace_back(node.overhang);
                             else {
-                                area = offset_ex({node.overhang}, scale_(m_ts_data->m_xy_distance));
+                                area = offset_ex({node.overhang}, scale_(orig_xy_distance));
                             }
                         } else {
                             Polygon circle(branch_circle);
