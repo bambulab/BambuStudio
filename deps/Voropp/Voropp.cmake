9b4fec96facd3cfd665f3726998a556760cbab5a
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2025-10-08 02:23:02 -0400

    Update Voropp source URL in CMake config
    Changed the Voropp project download URL to the new location at math.lbl.gov to ensure continued access to the dependency.

--- deps/Voropp/Voropp.cmake
+++ deps/Voropp/Voropp.cmake
@@ -1,5 +1,5 @@
 bambustudio_add_cmake_project(Voropp
-    URL "https://download.lbl.gov/voro++/voro++-0.4.6.tar.gz"
+    URL "https://math.lbl.gov/voro++/download/dir/voro++-0.4.6.tar.gz"
     URL_HASH SHA256=ef7970071ee2ce3800daa8723649ca069dc4c71cc25f0f7d22552387f3ea437e
     CMAKE_ARGS
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
