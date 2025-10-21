af72dc57b110c32bac6b6c24a97abe3a57457878
hemai <yuliang.zhang@bambulab.com>
2025-10-21 10:40:31 +0800

    ENH: support E3D for X1C
    Jira: [STUDIO-15007]
    Change-Id: I430ede776e6628bb43ff69980cd036769b1c16b0

--- src/slic3r/GUI/DeviceManager.cpp
+++ src/slic3r/GUI/DeviceManager.cpp
@@ -2379,7 +2379,6 @@ void MachineObject::reset()
         }
     }
     subtask_ = nullptr;
-    has_extra_flow_type = false;
     m_partskip_ids.clear();
 }
 
@@ -3417,7 +3416,6 @@ int MachineObject::parse_json(std::string tunnel, std::string payload, bool key_
                             if (jj.contains("flag3")) {
                                 int flag3           = jj["flag3"].get<int>();
                                 flag_e3d            = std::make_optional(get_flag_bits(flag3, 10, 3));
-                                has_extra_flow_type = true;
                             }
 
                             DevNozzleSystemParser::ParseV1_0(jj["nozzle_type"], jj["nozzle_diameter"], m_nozzle_system, flag_e3d);
