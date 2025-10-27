8518690f0445703a37a8709fd58d61503597ddca
zhou.xu <zhou.xu@bambulab.com>
2025-10-27 21:40:26 +0800

    ENH:alt key supports selecting multiple parts simultaneously and ctrl key supports selecting multiple objects simultaneously
    jira: STUDIO-13160
    Change-Id: Ic266d543737a4383ab97306f0fad526fa9c6d48e

--- src/slic3r/GUI/Gizmos/GLGizmosManager.hpp
+++ src/slic3r/GUI/Gizmos/GLGizmosManager.hpp
@@ -261,6 +261,7 @@ public:
     bool gizmo_event(SLAGizmoEventType action, const Vec2d& mouse_position = Vec2d::Zero(), bool shift_down = false, bool alt_down = false, bool control_down = false);
     bool is_paint_gizmo()const;
     bool is_allow_select_all() const;
+    bool is_allow_multi_select_parts_or_objects() const;
     bool is_allow_show_volume_highlight_outline() const;
     bool is_allow_drag_volume() const;
     bool is_allow_mouse_drag_selected() const;
