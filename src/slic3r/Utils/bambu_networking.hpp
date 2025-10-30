b1ba1c4a57ece80f51c4fdb39a37685e2c880add
xin.zhang <xin.zhang@bambulab.com>
2025-10-30 12:11:04 +0800

    NEW: support  extruder_cali_manual_mode
    JIRA: [STUDIO-15371]
    Change-Id: Ia2dfdcbd2f67aaf8047c074d83eebbedef18b39a

--- src/slic3r/Utils/bambu_networking.hpp
+++ src/slic3r/Utils/bambu_networking.hpp
@@ -234,6 +234,7 @@ struct PrintParams {
     int             auto_bed_leveling{ 0 };
     int             auto_flow_cali{ 0 };
     int             auto_offset_cali{ 0 };
+    int             extruder_cali_manual_mode{ -1 };
     bool            task_ext_change_assist;
     bool            try_emmc_print;
 };
