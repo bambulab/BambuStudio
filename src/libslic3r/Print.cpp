48187205d5fcd612f1fee47beaa411aac769e6bc
Michael Rook <michael@rook.id.au>
2025-12-04 19:49:20 +1100

    refactor: Add print validation for precise outer wall and wall sequence

--- src/libslic3r/Print.cpp
+++ src/libslic3r/Print.cpp
@@ -1535,7 +1535,14 @@ StringObjectException Print::validate(StringObjectException *warning, Polygons* 
                 return except;
             }
         }
-    }
+
+            // check wall sequence and precise outer wall
+            if (m_default_region_config.precise_outer_wall && m_default_region_config.wall_sequence != WallSequence::InnerOuter)
+            {
+                warning->string = L("Precise outer wall will be ignored unless the wall sequence is set to inner/outer");
+                warning->opt_key = "precise_outer_wall";
+            }
+        }
 
     return {};
 }
