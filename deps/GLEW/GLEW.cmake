75bd177e935d635a84bed0ee0cfc149762d0d53f
Bastien Nocera <hadess@hadess.net>
2026-01-08 11:20:42 +0100

    ENH: build: Use upstream GLEW
    Instead of our copy-paste version.

--- deps/GLEW/GLEW.cmake
+++ deps/GLEW/GLEW.cmake
@@ -4,7 +4,11 @@ find_package(OpenGL QUIET REQUIRED)
 
 bambustudio_add_cmake_project(
   GLEW
-  SOURCE_DIR  ${CMAKE_CURRENT_LIST_DIR}/glew
+  URL https://sourceforge.net/projects/glew/files/glew/2.2.0/glew-2.2.0.zip
+  URL_HASH SHA256=a9046a913774395a095edcc0b0ac2d81c3aacca61787b39839b941e9be14e0d4
+  SOURCE_SUBDIR build/cmake
+  CMAKE_ARGS
+    -DBUILD_UTILS=OFF
 )
 
 if (MSVC)
