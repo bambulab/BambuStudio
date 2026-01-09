a82f0b981e67885ab792f236c4839a178ab8a298
bdunks <brian.duncan@gmail.com>
2026-01-09 09:40:33 -0500

    FIX: Fix Gtk-Critical assertion in PresetComboBoxes
    1. Resolve the `gtk_cell_layout_get_cells: assertion GTK_IS_CELL_LAYOUT
    failed` error by switching from `gtk_cell_layout_get_cells()` to
    `gtk_container_get_children()` with direct PangoLayout ellipsization
    on GtkEntry widgets.
    
    Picked from PrusaSlicer:
    https://github.com/prusa3d/PrusaSlicer/commit/e855ab5d
    
    2. Add defensive guards for GTK widget sizing
    
        Fix Gtk-CRITICAL assertions in TextInput and DropDown widgets that
        occur when the widget attempt relative sizing against window size
        before GTK completes the window layout.
    
        Changes include:
        - TextInput.cpp: Clamp textSize.x >= -1 to prevent
          `gtk_widget_set_size_request` failures.
          - Picked from Prusa Slicer:
            https://github.com/prusa3d/PrusaSlicer/commit/e855ab5d
        - DropDown.cpp: Guard against zero/negative dimensions during early
          initialization:
          - Prevent szContent.x = 0 when parent size is unavailable.
            - Unique to OrcaSlicer
            - Triggers during initializing DesignerPanel --> BasicInfo -->
              License DropDown before a parent size is available.
            - Latent bug exists in Bambu Studio, but Bambu does not have
              License field enabled on this panel.
            - Created global guard for future resilience.
          - Clamp szContent.y >= 1 for dropdowns before content initialized.
            - Bug exists in BambuStudio
            - Prusa did not implement the "ENH: ComboBox Second DropDown"
    
    github pull request: https://github.com/bambulab/BambuStudio/pull/9379
    
    Change-Id: I374b8b7c5c28acba6f7fca4441b61c2f142f96b1

--- src/slic3r/GUI/Widgets/TextInput.cpp
+++ src/slic3r/GUI/Widgets/TextInput.cpp
@@ -239,6 +239,7 @@ void TextInput::DoSetSize(int x, int y, int width, int height, int sizeFlags)
         int unit_space = (m_unit.IsEmpty() ? 0 : unitSize.x + 5) + 10;
         wxSize textSize = text_ctrl->GetSize();
         textSize.x = size.x - textPos.x - labelSize.x - 10 - prefix_space;
+        if(textSize.x < -1) textSize.x = -1;
         text_ctrl->SetSize(textSize);
         text_ctrl->SetPosition({textPos.x + prefix_space, (size.y - textSize.y) / 2});
     }
