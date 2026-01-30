985ce4e612556c1cbb453533976e50a89d6586ec
songwei.li <songwei.li@bambulab.com>
2026-01-30 18:16:29 +0800

    FIX: extruder_id error for Acceleration feature for short movements
    unsigned int extruder_id = m_writer.filament()->extruder_id(); (extruder id)instead of unsigned int extruder_id = m_writer.filament()->id();(filament id)
    
    jira: STUDIO-16806
    Change-Id: I24ab900feb0e74ac8a40f3b3e0cffd735c959768

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -6485,7 +6485,7 @@ std::string GCode::travel_to(const Point &point, ExtrusionRole role, std::string
         bool use_short_travel_accel = false;
         if (!this->on_first_layer()) {
             // Check if short travel acceleration is enabled (value > 0)
-            unsigned int extruder_id = m_writer.filament()->id();
+            unsigned int extruder_id = m_writer.filament()->extruder_id();
             auto& short_accel = m_writer.get_travel_short_acceleration();
             if (extruder_id < short_accel.size() && short_accel[extruder_id] > 0) {
                 // Use short travel acceleration for external perimeters with short travel distance
