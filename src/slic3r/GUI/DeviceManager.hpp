1628b307d22ddb716f9dd9c3b277a89130efa993
changyu.chen <changyu.chen@bambulab.com>
2026-02-11 15:12:07 +0800

    NEW: to check if device supports active arc fitting
    jira: [STUDIO-15748]
    
    Change-Id: I06da33735da5aef89905bb75ab3dbd3096e1692c

--- src/slic3r/GUI/DeviceManager.hpp
+++ src/slic3r/GUI/DeviceManager.hpp
@@ -544,6 +544,7 @@ public:
     bool is_support_print_with_emmc{false};
     bool is_support_pa_mode{false};
     bool is_support_remote_dry = false;
+    bool is_support_active_arc_fitting{false};
 
     bool installed_upgrade_kit{false};
     int  bed_temperature_limit = -1;
