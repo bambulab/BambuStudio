e964f847bfa3613adb48698c0ab61e7fa14075b5
changyu.chen <changyu.chen@bambulab.com>
2026-03-02 10:41:39 +0800

    ENH: check if printer supports liveview preview before requesting
    jira: [STUDIO-17311]
    
    Change-Id: I3fdbaa5d7ca3ff2dab6f1adff4e31c85eae38aea

--- src/slic3r/GUI/DeviceManager.hpp
+++ src/slic3r/GUI/DeviceManager.hpp
@@ -548,6 +548,7 @@ public:
     bool is_support_pa_mode{false};
     bool is_support_remote_dry = false;
     bool is_support_active_arc_fitting{false};
+    bool is_support_liveview_preview{false};
 
     bool installed_upgrade_kit{false};
     int  bed_temperature_limit = -1;
