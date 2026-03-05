6a3ba0d2151ae6d418cbccf451c60227ec45cb5c
xin.zhang <xin.zhang@bambulab.com>
2026-03-05 20:21:37 +0800

    ENH: update DevFilaSwitch::IsReady
    jira: [NONE]
    Change-Id: I45ab31740f44b87a4ad6b33b141f5d676c0f5054

--- src/slic3r/GUI/DeviceCore/DevFilaSwitch.cpp
+++ src/slic3r/GUI/DeviceCore/DevFilaSwitch.cpp
@@ -5,13 +5,9 @@
 
 #include "slic3r/GUI/DeviceManager.hpp"
 
-namespace Slic3r
-{
+namespace Slic3r {
 
-DevFilaSwitch::DevFilaSwitch(MachineObject* owner)
-{
-    m_owner = owner;
-}
+DevFilaSwitch::DevFilaSwitch(MachineObject *owner) { m_owner = owner; }
 
 void DevFilaSwitch::Reset()
 {
@@ -27,15 +23,13 @@ void DevFilaSwitch::Reset()
 
 bool DevFilaSwitch::IsReady() const
 {
-    if (!m_is_installed) {
-        return false;
-    }
+    if (!m_is_installed) { return false; }
 
-    const auto& ams_list = m_owner->GetFilaSystem()->GetAmsList();
-    for (const auto& ams_item : ams_list) {
-        if (ams_item.second->GetBindedExtruderSet().size() == 0) {
-            return false;
-        }
+    const auto &ams_list = m_owner->GetFilaSystem()->GetAmsList();
+    if (ams_list.size() < m_owner->GetExtderSystem()->GetTotalExtderCount()) { return false; }
+
+    for (const auto &ams_item : ams_list) {
+        if (ams_item.second->GetBindedExtruderSet().size() == 0) { return false; }
     }
 
     return true;
@@ -44,9 +38,7 @@ bool DevFilaSwitch::IsReady() const
 std::optional<DevAmsTray> DevFilaSwitch::GetInA_Slot() const
 {
     auto ams_id_opt = GetInA_SlotId();
-    if (m_owner && ams_id_opt.has_value()) {
-        return m_owner->get_tray(std::to_string(ams_id_opt->first), std::to_string(ams_id_opt->first));
-    }
+    if (m_owner && ams_id_opt.has_value()) { return m_owner->get_tray(std::to_string(ams_id_opt->first), std::to_string(ams_id_opt->first)); }
 
     return std::nullopt;
 }
@@ -54,9 +46,7 @@ std::optional<DevAmsTray> DevFilaSwitch::GetInA_Slot() const
 std::optional<DevAmsTray> DevFilaSwitch::GetInB_Slot() const
 {
     auto ams_id_opt = GetInB_SlotId();
-    if (m_owner && ams_id_opt.has_value()) {
-        return m_owner->get_tray(std::to_string(ams_id_opt->first), std::to_string(ams_id_opt->first));
-    }
+    if (m_owner && ams_id_opt.has_value()) { return m_owner->get_tray(std::to_string(ams_id_opt->first), std::to_string(ams_id_opt->first)); }
 
     return std::nullopt;
 }
@@ -64,9 +54,7 @@ std::optional<DevAmsTray> DevFilaSwitch::GetInB_Slot() const
 std::optional<DevExtder> DevFilaSwitch::GetOutA_Extruder() const
 {
     auto ext_id_opt = GetOutA_ExtruderId();
-    if (m_owner && ext_id_opt.has_value()) {
-        return m_owner->GetExtderSystem()->GetExtderById(ext_id_opt.value());
-    }
+    if (m_owner && ext_id_opt.has_value()) { return m_owner->GetExtderSystem()->GetExtderById(ext_id_opt.value()); }
 
     return std::nullopt;
 }
@@ -74,45 +62,40 @@ std::optional<DevExtder> DevFilaSwitch::GetOutA_Extruder() const
 std::optional<DevExtder> DevFilaSwitch::GetOutB_Extruder() const
 {
     auto ext_id_opt = GetOutB_ExtruderId();
-    if (m_owner && ext_id_opt.has_value()) {
-        return m_owner->GetExtderSystem()->GetExtderById(ext_id_opt.value());
-    }
+    if (m_owner && ext_id_opt.has_value()) { return m_owner->GetExtderSystem()->GetExtderById(ext_id_opt.value()); }
 
     return std::nullopt;
 }
 
-
-void DevFilaSwitch::ParseFilaSwitchInfo(const nlohmann::json& print_jj)
+void DevFilaSwitch::ParseFilaSwitchInfo(const nlohmann::json &print_jj)
 {
     if (print_jj.contains("aux")) {
-        const auto& info_bits = DevJsonValParser::GetVal<std::string>(print_jj, "aux");
+        const auto &info_bits = DevJsonValParser::GetVal<std::string>(print_jj, "aux");
         if (m_is_installed != (DevUtil::get_flag_bits(info_bits, 29, 1) == 1)) {
             m_is_installed = (DevUtil::get_flag_bits(info_bits, 29, 1) == 1);
-            if (!m_is_installed) {
-                Reset();
-            }
+            if (!m_is_installed) { Reset(); }
         };
     }
 
     if (print_jj.contains("device") && print_jj["device"].contains("fila_switch")) {
-        const auto& fila_switch_jj = print_jj["device"]["fila_switch"];
+        const auto &fila_switch_jj = print_jj["device"]["fila_switch"];
         if (fila_switch_jj.contains("in")) {
-            const auto& in_vec = DevJsonValParser::GetVal<std::vector<int>>(fila_switch_jj, "in");
+            const auto &in_vec = DevJsonValParser::GetVal<std::vector<int>>(fila_switch_jj, "in");
             if (in_vec.size() == 2) {
                 if (in_vec[0] != -1) {
                     DevAmsSlotId slot_id;
-                    slot_id.first = DevUtil::get_flag_bits(in_vec[0], 8, 8);
+                    slot_id.first  = DevUtil::get_flag_bits(in_vec[0], 8, 8);
                     slot_id.second = DevUtil::get_flag_bits(in_vec[0], 0, 8);
-                    m_in_b_slot = slot_id;
+                    m_in_b_slot    = slot_id;
                 } else {
                     m_in_b_slot = std::nullopt;
                 };
 
                 if (in_vec[1] != -1) {
                     DevAmsSlotId slot_id;
-                    slot_id.first = DevUtil::get_flag_bits(in_vec[1], 8, 8);
+                    slot_id.first  = DevUtil::get_flag_bits(in_vec[1], 8, 8);
                     slot_id.second = DevUtil::get_flag_bits(in_vec[1], 0, 8);
-                    m_in_a_slot = slot_id;
+                    m_in_a_slot    = slot_id;
                 } else {
                     m_in_a_slot.reset();
                 };
@@ -120,7 +103,7 @@ void DevFilaSwitch::ParseFilaSwitchInfo(const nlohmann::json& print_jj)
         }
 
         if (fila_switch_jj.contains("out")) {
-            const auto& out_vec = DevJsonValParser::GetVal<std::vector<int>>(fila_switch_jj, "out");
+            const auto &out_vec = DevJsonValParser::GetVal<std::vector<int>>(fila_switch_jj, "out");
             if (out_vec.size() == 2) {
                 if (out_vec[0] != 0xE) {
                     m_out_b_extruder_id = out_vec[0];
@@ -136,16 +119,14 @@ void DevFilaSwitch::ParseFilaSwitchInfo(const nlohmann::json& print_jj)
             }
         }
 
-        if (fila_switch_jj.contains("stat")) {
-            m_cali_status = DevJsonValParser::GetVal<CaliStatus>(fila_switch_jj, "stat");
-        }
+        if (fila_switch_jj.contains("stat")) { m_cali_status = DevJsonValParser::GetVal<CaliStatus>(fila_switch_jj, "stat"); }
 
         if (fila_switch_jj.contains("info")) {
-            const auto& info_bits = DevJsonValParser::GetVal<int>(fila_switch_jj, "info");
-            m_in_b_has_filament = DevUtil::get_flag_bits(info_bits, 0, 1);
-            m_in_a_has_filament = DevUtil::get_flag_bits(info_bits, 0, 1);
+            const auto &info_bits = DevJsonValParser::GetVal<int>(fila_switch_jj, "info");
+            m_in_b_has_filament   = DevUtil::get_flag_bits(info_bits, 0, 1);
+            m_in_a_has_filament   = DevUtil::get_flag_bits(info_bits, 0, 1);
         }
     }
 }
 
-};
+}; // namespace Slic3r
