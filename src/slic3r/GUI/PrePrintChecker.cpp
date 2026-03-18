1b43503b8eee14307e98d7ef864f731db0cf8505
xingyun.xi <xingyun.xi@bambulab.com>
2026-03-18 11:29:24 +0800

    FIX: Sync with firmware, Studio does not require tips when using TPU for AMS
    jira: [STUDIO-16800]
    Change-Id: I96e8c0891753b3612859a58c3d58abba1831a3d8

--- src/slic3r/GUI/PrePrintChecker.cpp
+++ src/slic3r/GUI/PrePrintChecker.cpp
@@ -232,7 +232,7 @@ bool PrinterMsgPanel::UpdateInfos(const std::vector<prePrintInfo>& infos)
             label->SetForegroundColour(_GetLabelColour(info));
 
             if (info.wiki_url.empty())
-{
+            {
                 label->SetLabel(info.msg);
             }
             else
