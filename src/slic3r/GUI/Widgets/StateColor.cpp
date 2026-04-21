92c41613fb1d259cec3be7cc179d3a8df9b22058
baijie <tonghao.yuan@bambulab.com>
2026-04-21 19:07:15 +0800

    FIX: dark mode of ExtruderBadge JIRA: STUDIO-14720
    Change-Id: Ica050f21e6b58a3440a446fecab851ae1092c117

--- src/slic3r/GUI/Widgets/StateColor.cpp
+++ src/slic3r/GUI/Widgets/StateColor.cpp
@@ -41,7 +41,8 @@ static std::map<wxColour, wxColour> gDarkColors{
     {"#2B3436", "#808080"},
     {"#ABABAB", "#ABABAB"},
     {"#D9D9D9", "#2D2D32"},
-    {"#EBF9F0", "#293F34"}
+    {"#EBF9F0", "#293F34"},
+    {"#DBFDE7", "#1F3529"}
     //{"#F0F0F0", "#4C4C54"},
 };
 
