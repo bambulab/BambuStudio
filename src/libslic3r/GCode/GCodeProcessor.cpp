71a1302c201eafe1305a6622bd0f7827d4299ee6
wenbing.shao <wenbing.shao@bambulab.com>
2026-04-24 21:45:32 +0800

    FIX:切片预览的时候温度显示不正确
    原因：提前升降温时M104或者M109后面提供了T参数，控制的是另一个挤出机的温度，直接把温度设置成当前耗材的温度就会有问题。
    
    解决方案：解析的时候需要根据T参数找到对应的逻辑挤出机，再把挂载到该逻辑挤出机上的耗材更新相应的温度。
    固件里面不认R参数，为了降低维护成本故删掉R参数分支的处理。
    
    jira: STUDIO-18067
    
    Change-Id: I2b8b4c553d02f95eea1252d60cd16e7699cb1142

--- src/libslic3r/GCode/GCodeProcessor.cpp
+++ src/libslic3r/GCode/GCodeProcessor.cpp
@@ -5270,20 +5270,24 @@ void GCodeProcessor::process_M104(const GCodeReader::GCodeLine& line)
 {
     int filament_id = get_filament_id();
     float new_temp;
-    float   phy_extruder_id_temp;
+    if (!line.has_value('S', new_temp))
+        return;
+
+    float phy_extruder_id_temp;
     if (line.has_value('T', phy_extruder_id_temp)) {
+        // Map physical extruder id to logical extruder index
         int  phy_extruder_id_temp_int = std::round(phy_extruder_id_temp);
-        auto it_temp                  = std::find(m_physical_extruder_map.begin(), m_physical_extruder_map.end(), phy_extruder_id_temp_int);
-        if (line.has_value('S', new_temp) && it_temp != m_physical_extruder_map.end()) {
+        auto it_temp = std::find(m_physical_extruder_map.begin(), m_physical_extruder_map.end(), phy_extruder_id_temp_int);
+        if (it_temp != m_physical_extruder_map.end()) {
             size_t extruder_index = std::distance(m_physical_extruder_map.begin(), it_temp);
-            for (size_t ii = 0; ii < m_filament_maps.size(); ii++) {
-                auto it_temp_2 = std::find(m_filament_maps.begin(), m_filament_maps.end(), extruder_index);
-                size_t filament_index = std::distance(m_filament_maps.begin(), it_temp_2);
-                if (filament_index > 0 && filament_index < m_extruder_temps.size()) m_extruder_temps[filament_index] = new_temp;
+            // Update temperature for all filaments assigned to this logical extruder
+            for (size_t ii = 0; ii < m_filament_maps.size() && ii < m_extruder_temps.size(); ii++) {
+                if (static_cast<size_t>(m_filament_maps[ii]) == extruder_index)
+                    m_extruder_temps[ii] = new_temp;
             }
         }
     } else {
-        if (line.has_value('S', new_temp)) m_extruder_temps[filament_id] = new_temp;
+        m_extruder_temps[filament_id] = new_temp;
     }
 }
 
@@ -5338,20 +5342,7 @@ void GCodeProcessor::process_M108(const GCodeReader::GCodeLine& line)
 
 void GCodeProcessor::process_M109(const GCodeReader::GCodeLine& line)
 {
-    int filament_id = get_filament_id();
-    float new_temp;
-    if (line.has_value('R', new_temp)) {
-        float val;
-        if (line.has_value('T', val)) {
-            size_t eid = static_cast<size_t>(val);
-            if (eid < m_extruder_temps.size())
-                m_extruder_temps[eid] = new_temp;
-        }
-        else
-            m_extruder_temps[filament_id] = new_temp;
-    }
-    else if (line.has_value('S', new_temp))
-        m_extruder_temps[filament_id] = new_temp;
+    process_M104(line);
 }
 
 void GCodeProcessor::process_VM109(const GCodeReader::GCodeLine& line)
