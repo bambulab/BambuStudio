8d1408c461c2e593075a188d878a211c6b8b120e
Koos du Preez <35713435+kooscode@users.noreply.github.com>
2026-05-15 23:09:48 -0500

    Delete build-ubuntu.sh

--- build-ubuntu.sh
+++
@@ -1,7 +0,0 @@
-cd build
-rm -f CMakeCache.txt
-cmake .. -DSLIC3R_STATIC=ON -DSLIC3R_GTK=3 -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="/data/sources/BambuStudio/deps/build/destdir/usr/local" -DCMAKE_INSTALL_PREFIX="../install_dir" -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release -j32
-cmake --install .
-cd ..
-./BuildLinux.sh -i
