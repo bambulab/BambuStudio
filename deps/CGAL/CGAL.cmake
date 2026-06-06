3b1a92986d70f18877fec86393f8cee72a19d08f
Bastien Nocera <hadess@hadess.net>
2026-06-06 19:02:10 +0200

    ENH: Allow using system GMP and MPFR
    To make it easier to test newer versions, or apply system specific
    patches.
    
    In the case of Flatpak builds, GMP and MPFR are already available
    in the Freedesktop runtime.

--- deps/CGAL/CGAL.cmake
+++ deps/CGAL/CGAL.cmake
@@ -7,7 +7,7 @@ bambustudio_add_cmake_project(
     URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.4.zip
     URL_HASH SHA256=d7605e0a5a5ca17da7547592f6f6e4a59430a0bc861948974254d0de43eab4c0
     PATCH_COMMAND git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch
-    DEPENDS ${BOOST_PKG} dep_GMP dep_MPFR
+    DEPENDS ${BOOST_PKG} ${GMP_PKG} ${MPFR_PKG}
     CMAKE_ARGS
         -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 )
