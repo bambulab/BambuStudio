0a9e23d5f6717f3d78c4fe7750ee771d77ff0ef5
tangkelu <tangkelu123@gmail.com>
2026-06-08 07:48:16 +0800

    test: split filament state for apply validate smoke

--- src/libslic3r/PrintFilamentMapping.cpp
+++ src/libslic3r/PrintFilamentMapping.cpp
@@ -25,37 +25,6 @@ int Print::get_filament_config_indx(int filament_id, int layer_id)
     return get_config_index(filament_id, layer_id, m_config.filament_extruder_variant.values, m_filament_self_index, m_filament_index_map);
 }
 
-void Print::update_filament_self_index_cache()
-{
-    std::vector<int> values;
-    if (m_full_print_config.has("filament_self_index")) {
-        values = m_full_print_config.option<ConfigOptionInts>("filament_self_index")->values;
-    } else if (m_ori_full_print_config.has("filament_self_index")) {
-        values = m_ori_full_print_config.option<ConfigOptionInts>("filament_self_index")->values;
-    } else {
-        values = m_config.filament_self_index.values;
-    }
-
-    size_t expected_size = m_config.filament_extruder_variant.values.size();
-    m_filament_self_index.clear();
-    if (expected_size == 0) {
-        m_filament_index_map.clear();
-        m_nozzle_index_map.clear();
-        return;
-    }
-    m_filament_self_index.resize(expected_size, 1);
-    if (!values.empty()) {
-        for (size_t i = 0; i < expected_size; ++i) {
-            int v = i < values.size() ? values[i] : 1;
-            if (v <= 0)
-                v = 1;
-            m_filament_self_index[i] = v;
-        }
-    }
-    m_filament_index_map.clear();
-    m_nozzle_index_map.clear();
-}
-
 int Print::get_nozzle_config_index(int filament_id, int layer_id)
 {
     return get_config_index(filament_id, layer_id, m_config.print_extruder_variant.values, m_config.print_extruder_id.values, m_nozzle_index_map);
@@ -185,26 +154,6 @@ void Print::update_to_config_by_nozzle_group_result(const MultiNozzleUtils::Nozz
         m_config.apply(filament_overrides);
     }
     update_filament_self_index_cache();
-}
-
-std::vector<int> Print::get_filament_maps() const
-{
-    return m_config.filament_map.values;
-}
-
-std::vector<int> Print::get_filament_nozzle_maps() const
-{
-    return m_config.filament_nozzle_map.values;
-}
-
-std::vector<int> Print::get_filament_volume_maps() const
-{
-    return m_config.filament_volume_map.values;
-}
-
-FilamentMapMode Print::get_filament_map_mode() const
-{
-    return m_config.filament_map_mode;
 }
 
 std::vector<FilamentMapMode> Print::get_available_filament_map_modes() const
