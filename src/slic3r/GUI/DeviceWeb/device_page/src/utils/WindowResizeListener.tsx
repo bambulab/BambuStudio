8099a37abdd3b0226a746bf7786a95666161bb24
Aqi <jingyu.sun@bambulab.com>
2026-07-13 14:10:55 +0800

    FIX: text scaling issue in filament Manager on macOS during window resizing
    feishu: issue-7031220619
    
    Change-Id: I44bbdce743be25416201012a290c52a1231a0cbc

--- src/slic3r/GUI/DeviceWeb/device_page/src/utils/WindowResizeListener.tsx
+++ src/slic3r/GUI/DeviceWeb/device_page/src/utils/WindowResizeListener.tsx
@@ -1,13 +1,3 @@
-function setRemBase() {
-    const rootFontSize = 16; // root font size in pixels
-    document.documentElement.style.fontSize = window.devicePixelRatio*rootFontSize + 'px';
-}
-
-
-
-export default function WindowResizeListener(){
-    window.addEventListener('resize', setRemBase);
-    document.addEventListener('DOMContentLoaded', setRemBase);
-
+export default function WindowResizeListener() {
     return null;
 }
