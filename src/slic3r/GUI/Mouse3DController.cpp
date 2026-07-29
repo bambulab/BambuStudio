bambulab/BambuStudio

commit 1519fa69cb2e6b1a781b469f93d0b4a35bb2d952

Mouse3DController: Add SpaceMouse Wireless BT support
Update the device list to match the upstream spacenavd one.

src/slic3r/GUI/Mouse3DController.cpp
@@ -27,7 +27,7 @@ static const std::vector<int> _3DCONNEXION_VENDORS =
     0x256F   // 3DCONNECTION = 9583 // 3Dconnexion
 };
 
-// See: https://github.com/FreeSpacenav/spacenavd/blob/a9eccf34e7cac969ee399f625aef827f4f4aaec6/src/dev.c#L202
+// See: https://github.com/FreeSpacenav/spacenavd/blob/39856625a6de1e8c4b57c5938e1bf29d13cf1a9f/src/dev.c#L63
 static const std::vector<int> _3DCONNEXION_DEVICES =
 {
     0xc603,	/* 50691 spacemouse plus XT */
@@ -41,6 +41,7 @@ static const std::vector<int> _3DCONNEXION_DEVICES =
     0xc628,	/* 50728 space navigator for notebooks*/
     0xc629,	/* 50729 space pilot pro*/
     0xc62b,	/* 50731 space mouse pro*/
+    0xc640,	/* 50752 nulooq */
     0xc62e,	/* 50734 spacemouse wireless (USB cable) *TESTED* */
     0xc62f,	/* 50735 spacemouse wireless receiver */
     0xc631,	/* 50737 spacemouse pro wireless *TESTED* */
@@ -48,7 +49,7 @@ static const std::vector<int> _3DCONNEXION_DEVICES =
     0xc633,	/* 50739 spacemouse enterprise */
     0xc635,	/* 50741 spacemouse compact *TESTED* */
     0xc636,	/* 50742 spacemouse module */
-    0xc640,	/* 50752 nulooq */
+    0xc63a,	/* 60060 spacemouse wireless (Bluetooth) */
     0xc652, /* 50770 3Dconnexion universal receiver *TESTED* */
 };
 

commit 25e5d21ad9863a4f2373af81eb674f1c706087d1

FIX: Fix missing device name for SpaceMouse Wireless BT
Fixes: 1519fa69cb2e ("Mouse3DController: Add SpaceMouse Wireless BT support")

src/slic3r/GUI/Mouse3DController.cpp
@@ -136,6 +136,7 @@ static std::string format_device_string(int vid, int pid)
     case 0xc635: { ret += "spacemouse compact"; break; }
     case 0xc636: { ret += "spacemouse module"; break; }
     case 0xc638: { ret += "spacemouse pro wireless BT edition"; break; }
+    case 0xc63a: { ret += "spacemouse wireless (Bluetooth)"; break; }
     case 0xc640: { ret += "nulooq"; break; }
     case 0xc652: { ret += "3Dconnexion universal receiver"; break; }
     default:     { ret += "UNKNOWN"; break; }