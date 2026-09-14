fda3cb99009144fe4ac58a97a9c1d60768bea5ce
guilong.he <guilong.he@bambulab.com>
2026-09-14 14:38:06 +0800

    NEW: Add M marker to first-layer change temperature commands
    feishu: story-7113354493
    
    selftest: pass
    Change-Id: I3e70bb7d1859cc8a803bec63233f0e1ae8b40edc

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -4814,7 +4814,8 @@ GCode::LayerResult GCode::process_layer(
                 continue;
             int temperature = print.config().nozzle_temperature.get_at(extruder.id());
             if (temperature > 0 && temperature != print.config().nozzle_temperature_initial_layer.get_at(extruder.id()))
-                gcode += m_writer.set_temperature(temperature, false, extruder.id());
+                gcode += m_writer.set_temperature(
+                    temperature, false, extruder.id(), TemperatureCommandType::LayerChange);
         }
 
         // BBS
