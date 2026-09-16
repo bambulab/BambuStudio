ebe6942b6fa34091985e4c58eb0d9887f2790f2e
gerrit <gerrit@bambulab.com>
2026-09-16 17:20:58 +0800

    ci: update network module version 02.08.04.52 based on commit e992f5c 2026-09-16 17:20:57
    Change-Id: I9e29395fc6ad0e347d2b0bd44e7e273dd41c8c89

--- src/slic3r/Utils/bambu_networking.hpp
+++ src/slic3r/Utils/bambu_networking.hpp
@@ -134,7 +134,7 @@ namespace BBL {
 #define BAMBU_NETWORK_LIBRARY               "bambu_networking"
 #define BAMBU_NETWORK_AGENT_NAME            "bambu_network_agent"
 
-#define BAMBU_NETWORK_AGENT_VERSION         "02.08.04.51"
+#define BAMBU_NETWORK_AGENT_VERSION         "02.08.04.52"
 
 //iot preset type strings
 #define IOT_PRINTER_TYPE_STRING     "printer"
